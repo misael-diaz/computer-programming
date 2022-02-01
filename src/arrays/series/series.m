7c9c810f40ac29d6989a82d027de565cb1e83522
misael-diaz <misael.diaz@upr.edu>
2022-02-01 17:25:05 -0400

    improves series code documentation [MATLAB]
    COMMENTS:
    It also makes the program output more similar to that of its Python
    counterpart.

--- src/arrays/series/series.m
+++ src/arrays/series/series.m
@@ -4,101 +4,109 @@
 %
 % Synopsis:
 % Shows how to use arrays to compute (truncated) series in MATLAB.
-% Series examples: arithmetic series, sum of squares, alternating series,
-%                  geometric series, and Taylor series.
+%
+% Sinopsis:
+% Demuestra como usar arreglos para realizar computos de series en MATLAB.
+%
+% Examples:
+% [1] arithmetic series	[2] sum of squares	[3] alternating series
+% [4] geometric series 	[5] Taylor series	[6] Taylor series
+%
+%
+% Copyright (c) 2020 Misael Diaz-Maldonado
+% This file is released under the GNU General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+%
+% References:
+% [0] A Gilat, MATLAB: An Introduction with Applications, 6th edition.
 
+
+close all
 clear
 clc
 
-format long g
 
-
-
-fprintf("example 1: arithmetic sequence")
-% example 1:  arithmetic sequence
-%         for j = 1:100 sum(j) to obtain the sum of the
-% natural numbers from one to a hundred.
+fprintf('Example 1: Arithmetic Sequence\n')
+% Example 1:  Arithmetic Sequence
+% Obtains the sum of the natural numbers from one to a hundred:
+%
+%			for n = [1, 100], Sum(n)
+%
 
 % defines the index of the sequence
 n = 1:100;
 % computes and stores the elements of the sequence in an array
 sn = n;
 % answer, sums the elements of the series
-sum(sn)
+fprintf('for n = [1, 100], Sum(n) = %d\n\n\n', sum(sn) )
 
 
 
-fprintf("example 2: sum of squares")
-% example 2: sum of squares
-%         for n = 1:100 sum(n^2)
-% to obtain the sum of the squares of the natural numbers from
-% one to a hundred.
+fprintf('Example 2: Sum of Squares\n')
+% example 2: Sum of Squares
+% Obtains the sum of the squares of the natural numbers from
+% one to a hundred:
+%
+%			for n = [1, 100],  Sum(n^2)
+%
 
-% defines the index of the sequence
 n = 1:100;
-% computes and stores the elements of the sequence in an array
-sn = n.^2;
-% answer, sums the elements of the series
-sum(sn)
+sn = n.^2;	% NOTE: the dot `.' indicates elementwise operations
+fprintf('for n = [1, 100], Sum(n^2) = %d\n\n\n', sum(sn) )
 
 
 
-fprintf("example 3: alternating series")
-% example 3: alternating series
-%         for n = 0:99, sum( (-1)^(n) / (n+1) )
+fprintf('Example 3: Alternating Series\n')
+% example 3: Alternating Series
+%
+%         for n = [0, 100), Sum( (-1)^(n) / (n+1) )
+%
 
-% defines the index of the sequence
-n = 0:99;
-% computes and stores the elements of the sequence in an array
+n = 0:99;	% NOTE: in MATLAB the end of the range is inclusive
 sn = (-1).^(n) ./ (n+1);
-% answer, sums the elements of the series
-sum(sn)
+fprintf('for n = [0, 100), Sum( (-1)^n / (n + 1) ) = %.6f\n\n\n', sum(sn) )
 
 
 
-fprintf("example 4: geometric series")
-% example 4: geometric series
-%         for n = 0:99 sum(r^n), where r = 0.2
-% defines the geometric constant
-r = 0.2;
-% defines the index of the sequence
+fprintf('Example 4: Geometric Series\n')
+% example 4: Geometric Series
+%
+% 	given r = 0.2, 	for n = [0, 100), Sum(r^n)
+%
+
+r = 0.2;	% defines the geometric constant `r'
 n = 0:99;
-% computes and stores the elements of the sequence in an array
 sn = r.^n;
-% answer, sums the elements of the series
-sum(sn)
+fprintf('for n = [0, 100), Sum(r^n) = %.6f\n\n\n', sum(sn) )
 
 
 
-fprintf("example 5: Taylor series")
-% example 5: Taylor series,
-% compute the first 16 elements of the series
-%         for n = 0:15 sum(2^n / n!),
-% where n! is the factorial of n
+fprintf('Example 5: Taylor Series\n')
+% Example 5: Taylor series
+% Computes the first 16 elements of the following Taylor series:
+%
+%         	for n = [0, 16), Sum(2^n / n!),
+%
+% where `n!' is the factorial of n.
+%
 
-% note that the index n = 0:15 has 16 elements,
-% you may want to check that numel(n) returns 16 elements
-% when invoked on the command window
-
-% defines the index of the series
 n = 0:15;
-% computes and stores the elements of the sequence in an array
-sn = 2.^n ./factorial(n);
-% answer, sums the elements of the series
-sum(sn)
+sn = 2.^n ./ factorial(n);
+fprintf('for n = [0, 16), Sum(2^n / n!) = %.6f\n\n\n', sum(sn) )
 
 
 
-fprintf("example 6: Taylor series")
-% example 6: Taylor series, compute the first 11 elements of the series
-%          for n = 0:10 sum( (-1)^n * x^(2n+1) / (2n+1)! )
+fprintf('Example 6: Taylor series\n')
+% Example 6: Taylor series
+% Computes the first 11 elements of the Taylor series:
+%
+%		for n = [0, 11), Sum( (-1)^n * x^(2n+1) / (2n+1)! )
+%
 
-% defines the independent variable
 x = pi/3;
-% defines the index of the series
 n = 0:11;
-% computes and stores the elements of the sequence in an array
-sn = (-1).^n .* x.^(2*n+1) ./factorial(2*n+1);
-% answer, sums the elements of the series
-sum(sn)
-
+sn = (-1).^n .* x.^(2*n+1) ./ factorial(2*n+1);
+fprintf(['for n = [0, 11), ', ...	% NOTE: uses string concatenation
+	 'Sum( (-1)^n * x^(2n+1) / (2n+1)! ) = %.6f\n\n\n'], sum(sn) )
