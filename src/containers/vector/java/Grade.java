679b8d8f976c0d91a41b930c784ed33a026e954c
misael-diaz <misael.diaz@upr.edu>
2022-04-28 10:13:14 -0400

    accounts for unsubmitted homeworks [Java]
    COMMENTS:
    Inserts zeros for unsubmitted homeworks in the homeworks vector for
    the computation of the median grade.

--- src/containers/vector/java/Grade.java
+++ src/containers/vector/java/Grade.java
@@ -73,7 +73,7 @@ public class Grade
 
 
 			int g;		// grade
-			int min = 3;	// min number of homeworks done
+			int min = 4;	// min number of homeworks done
 			int max = 8;	// max number of homeworks done
 			int hws;	// number of homeworks done
 			int homework;	// homework score
@@ -90,7 +90,7 @@ public class Grade
 				hws = min + homeworks.nextInt(max - min);
 				for (int j = 0; j != hws; ++j)
 				{
-					g = 60 + grade.nextInt(40);
+					g = 50 + grade.nextInt(45);
 					out.printf("%4d", g);
 				}
 				out.println();
@@ -119,6 +119,7 @@ public class Grade
 
 			String student;
 			int grade;
+			int missed;
 
 			// reads until the end-of-file EOF
 			while ( in.hasNext() )
@@ -134,6 +135,12 @@ public class Grade
 					homeworks.push_back(homeworks,
 							    in.nextInt());
 				}
+
+				// inserts score for unsubmitted homework
+				missed = 7 - homeworks.size(homeworks);
+				for (int i = 0; i != missed; ++i)
+					homeworks.push_back(homeworks, 0);
+
 				// obtains the median homework grade
 				grade = homeworks.median(homeworks);
 				// prints student name and grade
