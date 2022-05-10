917192c7a067b75bdfd97bc34f549c274af9de7a
misael-diaz <misael.diaz@upr.edu>
2022-05-09 20:47:45 -0400

    implements Non-recursive Divide and Conquer Method
    COMMENTS:
    Finds the closest pair in datasets consisting of exactly 12 (x, y)
    coordinates.
    
    It does it job by finding the closest pairs in the quadrants and
    then it looks for closest pairs near the middles of the system.

--- src/algorithms/divide-and-conquer/ClosestPair.java
+++ src/algorithms/divide-and-conquer/ClosestPair.java
@@ -27,16 +27,41 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Random;
 import static java.lang.Math.sqrt;
+import static java.lang.Math.ceil;
 
 class ClosestPair_DivideAndConquerAlgorithm
 {
 
 	public static void main (String[] args)
 	{
+
+		/* initial examples have been disabled */
+
+
 		// divides into left and right subsets
-		halves ();
+		//halves ();
 		// divides into quadrants and finds the closest pairs
-		quadrants ();
+		//quadrants ();
+
+
+		/* Non-recursive Divide and Conquer Algorithm */
+		// NOTE: Applies for datasets consisting of 12 coordinates
+
+
+		Vector P = Dataset();
+		int [][] closestPair = new int [2][2];
+		double d_min = ClosestPair (P, closestPair);
+
+		// prints coordinates of the closest pair
+		Vector.Coord first  = new Vector.Coord( closestPair[0] );
+		Vector.Coord second = new Vector.Coord( closestPair[1] );
+
+		System.out.println("\n");
+		System.out.printf("distance: %.4f\n", d_min);
+		System.out.printf("coordinates:\n");
+		first.print(first);
+		second.print(second);
+		System.out.println("\n");
 
 		return;
 	}
@@ -159,6 +184,65 @@ class ClosestPair_DivideAndConquerAlgorithm
 	}
 
 
+	private static
+	double distance (Vector coords1, Vector coords2, int [][] Pair)
+	// Finds the Closest Pair near boundaries with Brute Force.
+	// The dataset `coords1' contains the particles in the left (lower)
+	// subset and the dataset `coords2' contains the particles in the
+	// right (upper) subset. The method finds the closest pair by
+	// comparing the distance of the particles in the Left subset
+	// with those on the Right subset. Note that at this point the
+	// algorithm has considered the distances within the quadrants,
+	// thus all that's left to do is consider the boundaries as it is
+	// done here.
+	{
+		// complains if the 2nd-rank array is not 2 x 2
+		int [][] closestPair = Pair;
+		shape (closestPair);
+
+		double d;			// distance from P to Q
+		double x_p, y_p;		// x, y coords of P
+		double x_q, y_q;		// x, y coords of Q
+		int [] P = new int [2];		// x, y coords of P
+		int [] Q = new int [2];		// x, y coords of Q
+
+		int N = coords1.size (coords1);
+		int M = coords2.size (coords2);
+		double d_min = Double.MAX_VALUE;// uses maximum double
+		for (int i = 0; i != N; ++i)
+		{
+			for (int j = 0; j != M; ++j)
+			{
+				// gets coordinates of P and Q
+				P = coords1.index (coords1, i);
+				Q = coords2.index (coords2, j);
+
+				// unpacks coordinates
+				x_p = ( (double) P[0] );
+				y_p = ( (double) P[1] );
+
+				x_q = ( (double) Q[0] );
+				y_q = ( (double) Q[1] );
+
+				// computes the squared distance for speed
+				d = (x_p - x_q) * (x_p - x_q) +
+				    (y_p - y_q) * (y_p - y_q);
+
+				if (d < d_min)
+				{
+					d_min = d;
+					closestPair[0] = P;
+					closestPair[1] = Q;
+				}
+			}
+
+		}
+
+		return sqrt(d_min);	// returns min distance
+	}
+
+
+
 	private static double distance (int b, int e, Vector coords,
 					int [][] closestPair)
 	// finds the Closest Pair in [b, e) via the Brute Force Algorithm
@@ -269,13 +353,8 @@ class ClosestPair_DivideAndConquerAlgorithm
 		// stores original dataset P as the Left subset, Lx
 		Vector Lx = Dataset();
 
-		System.out.println("\noriginal dataset:\n");
-		Lx.print(Lx);
-
-		System.out.println("\nsorted dataset:\n");
 		// sorts original dataset to obtain Px
 		Lx.sort (Lx);
-		Lx.print(Lx);
 
 		// pops elements of Px in the range [m, e) to obtain Rx:
 		int size = Lx.size (Lx);		// size
@@ -286,14 +365,33 @@ class ClosestPair_DivideAndConquerAlgorithm
 		// fits vectors to size to reduce memory usage
 		Lx.fit (Lx);
 		Rx.fit (Rx);
+	}
 
-		// prints Left subset on the console
-		System.out.println("\nLx:\n");
-		Lx.print (Lx);
 
-		// prints Right subset on the console
-		System.out.println("\nRx:\n");
-		Rx.print (Rx);
+	private static Vector halves (Vector Px)
+	// divides dataset into left (lower) and right (upper) subsets
+	{
+		return build_xySortedHalves (Px);
+	}
+
+
+	private static Vector build_xySortedHalves (Vector Px)
+	// divides dataset and returns ``Left'' and ``Right'' subsets
+	{
+		// creates reference to original dataset (or alias)
+		Vector Lx = Px;
+		Vector Rx = new Vector();
+
+		// pops elements of Px in the range [m, e) to obtain Rx:
+		int size = Px.size (Px);		// size
+		int b = (size / 2), e = size;		// middle and end
+		Rx.push_back (Rx, Px.pop(Px, b, e) );	// pops by range
+
+		// fits vectors to size to reduce memory usage
+		Lx.fit (Lx);
+		Rx.fit (Rx);
+
+		return Rx;
 	}
 
 
@@ -491,5 +589,447 @@ class ClosestPair_DivideAndConquerAlgorithm
 		System.out.printf("coordinates:\n");
 		first.print(first);
 		second.print(second);
+	}
+
+
+	private static Vector build_yxSortedHalves (Vector Py, Vector Rx)
+	/*
+	 * Synopsis:
+	 * Constructs y-x sorted, Left (Ly) and Right (Ry) subsets by
+	 * popping elements in Py (a y-x sorted dataset) that are present
+	 * in Rx (x-y sorted Right subset). The construction is carried out
+	 * so that no sorting of Ly and Ry is not required, that is the y-x
+	 * ordering of Py is preserved.
+	 *
+	 *
+	 * Inputs:
+	 * Py		y-x sorted dataset
+	 * Rx		x-y sorted, right, subset
+	 *
+	 * Outputs:
+	 * Ly		y-x sorted, left, subset
+	 * Ry		y-x sorted, right, subset
+	 *
+	 *
+	 * Note that Ly is just a reference to Py so that the elements
+	 * that remain in Py after the popping are the elements in Ly.
+	 * (Thus it is not necessary to create another copy just a
+	 * reference as is done here to save space.)
+	 *
+	 */
+	{
+		Vector Ly = Py;			// creates alias for Py
+		Vector Ry = new Vector();	// placeholder for Ry
+
+		int pos;			// positional index
+		int idx = 0;			// element index
+		int size = Ly.size (Ly);	// number of elements
+		int [] coord = new int [2];	// coordinate placeholder
+		// pops elements in Py matching Rx while inserting into Ry
+		// to obtain both Ly and Ry subsets
+		for (int n = 0; n != size; ++n)
+		// NOTE: pops elements in Py in order (beginning to end)
+		//       so that Ry ends up with the same ordering of Py
+		{
+			coord = Ly.index(Ly, idx);
+			// Note that when popping elements from beginning
+			// to end it is not necessary to increment the
+			// index when an element is popped because the next
+			// element in the vector ends up in the popped
+			// location. On the other hand, if the current
+			// element is not popped, the index needs to be
+			// incremented to consider the next element on the
+			// next iteration.
+			pos = Rx.search(Rx, coord);
+			if (pos != 0)
+				Ry.push_back( Ry, Ly.pop(Ly, coord) );
+			else
+				++idx;
+		}
+
+		// fits vectors to size to reduce memory usage
+		Ly.fit(Ly);
+		Ry.fit(Ry);
+
+		return Ry;
+	}
+
+
+	private static
+	double dirClosestPairHalf (Vector P, int [][] Pair)
+	// Uses the direct method to find the closest pair in a half subset
+	{
+		Vector coords = P;
+		int [][] closestPair = Pair;
+		return distance (coords, closestPair);
+	}
+
+
+	private static
+	double dirClosestPairQuad (int b, int e, Vector P, int [][] Pair)
+	// Uses the direct method to find the closest pair in a quadrant
+	{
+		Vector coords = P;
+		int [][] closestPair = Pair;
+		return distance (b, e, coords, closestPair);
+	}
+
+
+	private static
+	double dirClosestPairQuad (Vector P, int [][] closestPair)
+	/*
+	 * Synopsis:
+	 * Uses the direct solution to determine the closest pair in the
+	 * quadrants I (II) and IV (III). Note that the method is meant
+	 * to used on the Left subset Ly or the Right subset Ry.
+	 *
+	 */
+	{
+		// complains if the direct solution should not be applied
+		is_direct (P);
+
+		// creates placeholders for closest pairs in the quadrants
+		int [][] closestPairQuadL = new int[2][2];	// lower
+		int [][] closestPairQuadU = new int[2][2];	// upper
+		int [][] closestPairQuadB = new int[2][2];	// boundary
+		// creates a shallow copy for working on the middle section
+		Vector V = P.copy (P);
+
+
+		// divides so that there are three (or fewer) particles
+		int b = 0, m = (P.size(P) / 2), e = P.size(P);
+		// finds closest pair in quadrants I (II) and IV (III)
+		double dL = dirClosestPairQuad (b, m, P, closestPairQuadL);
+		double dU = dirClosestPairQuad (m, e, P, closestPairQuadU);
+
+
+		double minDistQuads;
+		// selects the closest of the closest pairs
+		if (dL < dU)
+		{
+			minDistQuads = dL;
+
+			// copies x, y coordinates of first particle
+			closestPair[0][0] = closestPairQuadL[0][0];
+			closestPair[0][1] = closestPairQuadL[0][1];
+
+			// copies x, y coordinates of second particle
+			closestPair[1][0] = closestPairQuadL[1][0];
+			closestPair[1][1] = closestPairQuadL[1][1];
+		}
+		else
+		{
+			minDistQuads = dU;
+
+			// copies x, y coordinates of first particle
+			closestPair[0][0] = closestPairQuadU[0][0];
+			closestPair[0][1] = closestPairQuadU[0][1];
+
+			// copies x, y coordinates of second particle
+			closestPair[1][0] = closestPairQuadU[1][0];
+			closestPair[1][1] = closestPairQuadU[1][1];
+		}
+
+		return minDistQuads;
+	}
+
+
+	private static
+	double ClosestPairBound (Vector V, int [][] closestPair,
+				 int axis, int d_min)
+	/*
+	 * Synopsis:
+	 * Looks for the closest pair near the boundaries. It does it work
+	 * by dividing the dataset V into left (lower) and right (upper)
+	 * subsets. Then it prunes elements in those subsets which are too
+	 * far to be closest-pair candidates. If either of the subsets does
+	 * not have any elements after pruning (empty) the method returns
+	 * the maximum double; otherwise, it returns the distance of the
+	 * closest pair between the left (lower) and right (upper) subsets.
+	 * The method is generic in the sense that it can be used to look
+	 * for closest pairs between the left and right halves (axis == 0)
+	 * and between the lower and upper halves (axis == 1).
+	 *
+	 * Inputs:
+	 * V		shallow copy of a Px (or Py) dataset
+	 * closestPair	uninitialized 2nd-rank array
+	 * axis		if axis == 0, the x-axis; if axis == 1, the y-axis
+	 * d_min	integer greater than or equal to the min distance
+	 *
+	 * Outputs:
+	 * min		smallest distance of particles near boundary
+	 * closestPair	coordinates of the respective particles
+	 *
+	 */
+	{
+		String errmsg = ("bound(): axis must be zero or one");
+		if (axis < 0 || axis > 1)
+			throw new RuntimeException(errmsg);
+
+		int d;	// distance
+		int ax;	// either the x or y-axis coordinate
+
+		// gets first half, Left (or lower) subset
+		Vector L = V;
+		// pops the second half into the Right (or upper) subset
+		Vector R = halves (V);
+
+
+		/* prunes particles on the Left (or lower) subset */
+
+		int [] coord;
+		// gets coordinates of the particle closest to boundary
+		// NOTE: Selects the first particle in the Right (upper)
+		//       half is for it is closer to those in the Left
+		//       (lower) half.
+		int [] middle = R.index (R, 0);
+		// gets the x or y-coordinate of the selected particle
+		int ax_m = middle[axis];
+
+		int idx = 0;
+		int size = L.size (L);
+		for (int n = 0; n != size; ++n)
+		// pops particles farther than the closest pair in `Left'
+		// NOTE: vector is sorted so no need to traverse it fully
+		//       and no need to update index since pop does that
+		//       behind scenes.
+		{
+			// gets x (y) coordinate of the referenced particle
+			coord = L.index (L, idx);
+			ax = coord[axis];
+
+			d = (ax > ax_m)? (ax - ax_m): (ax_m - ax);
+			// calculates distance (magnitude) and prunes those
+			// too far to be closest-pair candidates
+			if (d > d_min)
+				L.pop (L, coord);
+			else
+				break;
+		}
+
+
+		/* prunes particles on the Right (or upper) subset */
+
+		// pops elements from the back of vector since these are
+		// known to be farther from the Left (lower) boundary
+		idx = (R.size (R) - 1);
+		// gets coordinates of the particle closest to boundary
+		// NOTE: The last particle in the Left (lower) half is
+		//       closer to the boundary.
+		middle = L.index (L, L.size(L) - 1);
+		// gets the x or y-coordinate of the selected particle
+		ax_m = middle[axis];
+		size = R.size (R);
+		for (int n = 0; n != size; ++n)
+		// pops particles farther than the closest pair in `Right'
+		{
+			coord = R.index (R, idx);
+			ax = coord[axis];
+
+			// calculates the distance and prunes those too far
+			// to be closest-pair candidates
+			d = (ax > ax_m)? (ax - ax_m): (ax_m - ax);
+			if (d > d_min)
+			{
+				R.pop (R, coord);
+				// decrements index to consider the next
+				// from back to front of the vector
+				--idx;
+			}
+			else
+				break;
+		}
+
+		// returns the closest pair in the boundary if there is any
+		// otherwise returns infinity to signal the caller that
+		// there were no pairs closer than the current one.
+		if ( L.size(L) != 0 && R.size(R) != 0 )
+			return distance (L, R, closestPair);
+		else
+			return Double.MAX_VALUE;
+	}
+
+
+	private static double ClosestPair (Vector P, int [][] closestPair)
+	/*
+	 * Synopsis:
+	 * Closest Pair Method.
+	 * Applies the direct solution (or Brute Force Method) if available,
+	 * otherwise delegates the task to the method that implements the
+	 * Divide and Conquer Algorithm.
+	 *
+	 * Inputs:
+	 * P		vector, a container of the dataset of coordinates
+	 * closestPair	uninitialized 2nd-rank array
+	 *
+	 * Outputs:
+	 * d_min	distance of the closest pair
+	 * closestPair	2nd-rank array holding the coordinates of the pair
+	 *
+	 */
+	{
+
+		String errmsg = ("ClosestPair(): no pairs to compare");
+		if (P.size (P) <= 1)
+			throw new RuntimeException (errmsg);
+
+		if (P.size (P) <= 3)
+
+			return distance (P, closestPair);
+
+		else
+		{
+			Vector Px = P.copy (P);		// x-y sorted data
+			Vector Py = P.copy (P);		// y-x sorted data
+
+			Px.sort (Px);			// x-y sorts
+			Py.sort (Py, Py.comparator);	// y-x sorts
+
+			// invokes divide and conquer implementation
+			return findClosestPair (Px, Py, closestPair);
+		}
+	}
+
+
+	private static
+	double findClosestPair (Vector Px, Vector Py, int [][] closestPair)
+	// implements non-recursive divide and conquer algorithm
+	{
+
+
+		/* assumes that dividing into halves is required: */
+
+
+		// creates a shallow copies for working on the boundaries
+		Vector Mx = Px.copy (Px);
+		Vector My = Py.copy (Py);
+
+		// builds x-y sorted Left and Right subsets
+		Vector Rx = build_xySortedHalves (Px);
+		Vector Lx = Px;
+
+		// prints Left x-y sorted subset on the console
+		System.out.println("\nLx:\n");
+		Lx.print (Lx);
+
+		// prints Right x-y sorted subset on the console
+		System.out.println("\nRx:\n");
+		Rx.print (Rx);
+
+
+		/* assumes the direct method is applicable on quadrants: */
+
+
+		// builds y-x sorted Left and Right subsets
+		Vector Ry = build_yxSortedHalves (Py, Rx);
+		Vector Ly = Py;
+
+		// prints Left y-x sorted subset on the console
+		System.out.println("\nLy:\n");
+		Ly.print (Ly);
+
+		// prints Right y-x sorted subset on the console
+		System.out.println("\nRy:\n");
+		Ry.print (Ry);
+
+
+		double minDistLeft;
+		double minDistRight;
+		int [][] closestPairLeft   = new int [2][2];
+		int [][] closestPairRight  = new int [2][2];
+		// finds closest pair in quadrants via direct method
+		minDistLeft  = dirClosestPairQuad (Ly, closestPairLeft);
+		minDistRight = dirClosestPairQuad (Ry, closestPairRight);
+
+
+		double minDistQuads;
+		// selects the closest pair from the quadrants
+		if (minDistLeft < minDistRight)
+		{
+			minDistQuads = minDistLeft;
+
+			closestPair[0][0] = closestPairLeft[0][0];
+			closestPair[0][1] = closestPairLeft[0][1];
+
+			closestPair[1][0] = closestPairLeft[1][0];
+			closestPair[1][1] = closestPairLeft[1][1];
+		}
+		else
+		{
+			minDistQuads = minDistRight;
+
+			// copies x, y coordinates of first particle
+			closestPair[0][0] = closestPairRight[0][0];
+			closestPair[0][1] = closestPairRight[0][1];
+
+			// copies x, y coordinates of second particle
+			closestPair[1][0] = closestPairRight[1][0];
+			closestPair[1][1] = closestPairRight[1][1];
+		}
+
+
+		/* looks for the closest pair near the boundaries: */
+
+
+		int axis = 1;	// selects y-axis coordinates
+		int ceilMinDistQuads = ( (int) ceil(minDistQuads) );
+		int [][] closestPairQuads = new int [2][2];
+		// considers the Lower and Upper halves
+		double minDist = ClosestPairBound (My, closestPairQuads,
+				                   axis, ceilMinDistQuads);
+
+		double d_min = minDistQuads;
+		// updates the closest pair
+		if (minDist < minDistQuads)
+		{
+			d_min = minDist;
+
+			// copies x, y coordinates of first particle
+			closestPair[0][0] = closestPairQuads[0][0];
+			closestPair[0][1] = closestPairQuads[0][1];
+
+			// copies x, y coordinates of second particle
+			closestPair[1][0] = closestPairQuads[1][0];
+			closestPair[1][1] = closestPairQuads[1][1];
+		}
+
+
+
+		axis = 0;	// selects x-axis coordinates
+		int ceilMinDist = ( (int) ceil(d_min) );
+		int [][] closestPairHalves = new int [2][2];
+		// considers the Left and Right halves
+		minDist = ClosestPairBound (Mx, closestPairHalves,
+				            axis, ceilMinDist);
+
+		// updates the closest pair
+		if (minDist < d_min)
+		{
+			d_min = minDist;
+
+			// copies x, y coordinates of first particle
+			closestPair[0][0] = closestPairHalves[0][0];
+			closestPair[0][1] = closestPairHalves[0][1];
+
+			// copies x, y coordinates of second particle
+			closestPair[1][0] = closestPairHalves[1][0];
+			closestPair[1][1] = closestPairHalves[1][1];
+		}
+
+		return d_min;
+	}
+
+
+	private static void is_direct (Vector P)
+	// Complains if the direct solution cannot be applied as is
+	{
+		int size = P.size (P);
+		String errmsg = ("direct(): #particles is less than two");
+		if (size < 2)
+			throw new RuntimeException (errmsg);
+
+		errmsg = ("direct(): #particles is greater than six");
+		if (size > 6)
+			throw new RuntimeException (errmsg);
 	}
 }
