dc51c11587493166d4df572553ec0ac385fb5417
misael-diaz <misael.diaz@upr.edu>
2022-05-31 23:57:46 -0400

    fixes sticky player movement bug
    COMMENTS:
    From Let's Build a Game Tutorial 7.
    
    Fixes the sticky player movement bug.
    
    Pressing opposing arrow keys quickly caused the player to stick
    temporarily in place making it vulnerable to enemy attacks. Now
    the user can control the player more fluently.

--- src/games/tutorial/KeyInput.java
+++ src/games/tutorial/KeyInput.java
@@ -29,6 +29,8 @@ public class KeyInput extends KeyAdapter
 // defines the key input class for processing input from the player
 {
 
+	// we use this boolean array to avert sticky player movements
+	private boolean [] keyDown = {false, false, false, false};
 	// as for other objects this class needs a handler
 	private Handler handler;
 
@@ -69,15 +71,19 @@ public class KeyInput extends KeyAdapter
 		// sets velocity according to pressed key
 		{
 			case KeyEvent.VK_UP:
+				keyDown[0] = true;
 				object.setVelY (-5);
 				break;
 			case KeyEvent.VK_DOWN:
+				keyDown[1] = true;
 				object.setVelY (+5);
 				break;
 			case KeyEvent.VK_LEFT:
+				keyDown[2] = true;
 				object.setVelX (-5);
 				break;
 			case (KeyEvent.VK_RIGHT):
+				keyDown[3] = true;
 				object.setVelX (+5);
 				break;
 			case (KeyEvent.VK_SPACE):
@@ -110,20 +116,28 @@ public class KeyInput extends KeyAdapter
 		// resets velocity to default (stationary)
 		{
 			case KeyEvent.VK_UP:
-				object.setVelY (0);
+				keyDown[0] = false;
 				break;
 			case KeyEvent.VK_DOWN:
-				object.setVelY (0);
+				keyDown[1] = false;
 				break;
 			case KeyEvent.VK_LEFT:
-				object.setVelX (0);
+				keyDown[2] = false;
 				break;
 			case (KeyEvent.VK_RIGHT):
-				object.setVelX (0);
+				keyDown[3] = false;
 				break;
 			case (KeyEvent.VK_SPACE):
 				break;
 		}
+
+		// zeroes vertical movement
+		if (!keyDown[0] && !keyDown[1])
+			object.setVelY (0);
+
+		// zeroes horizontal movement
+		if (!keyDown[2] && !keyDown[3])
+			object.setVelX (0);
 	}
 }
 
@@ -136,5 +150,9 @@ public class KeyInput extends KeyAdapter
  *
  * By design we expect the player to be at the front of the linked-list
  * (that is, to be the first object in the linked-list).
+ *
+ * We set the player velocity to zero only if the user is not pressing
+ * opposing arrow keys consecutively to avoid the sticky effect bug.
+ * Now the user can quickly press down opposing keys for tight maneuvers.
  *
  */
