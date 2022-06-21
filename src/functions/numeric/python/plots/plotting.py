b1a17693d1190b3346d6d34e97bc39b5623cf413
misael-diaz <misael.diaz@upr.edu>
2022-06-21 16:19:43 -0400

    adds comments [Python]

--- src/functions/numeric/python/plots/plotting.py
+++ src/functions/numeric/python/plots/plotting.py
@@ -24,6 +24,7 @@ References:
     Science Applications with NumPy, SciPy, and Matplotlib, 2nd edition
 """
 
+
 from numpy import sin
 from numpy import array
 from numpy import linspace
@@ -31,28 +32,40 @@ from numpy import zeros_like
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
+
+# defines x as an array with values in the range [0, 5] for plotting
 x = linspace(0, 5, 100)
 # defines the nonlinear function f(x) as a lambda (or anonymous) function
 f = lambda x: 8 - 4.5 * ( x - sin(x) )
+# evaluates the function
 y = f(x)
 
-# defines possible bracketing interval
+
+# defines possible bracketing interval (lower and upper bounds)
 lb, ub = (2.4, 2.5)
+# stores the bracketing interval in an array for plotting
 xi = array([lb, ub])
 yi = f(xi)
+
 
 plt.close('all')
 plt.ion()
 fig, ax = plt.subplots()    # enables multiple plots in the same figure
+# plots the function f(x)
 ax.plot(x, y, label='f(x)')
+# plots the bracketing interval
 ax.plot(xi, yi, linestyle='', color='red', marker='o', label='bracketing')
 # plots a zero line (optional)
 ax.plot(x, zeros_like(x), linestyle='--', color='black')
+# sets limits for the axes
 ax.set_xlim([ 2, 3])
 ax.set_ylim([-3, 3])
+# sets labels for the axes
 ax.set_xlabel('x')
 ax.set_ylabel('f(x)')
+# shows the grid
 ax.grid()
+# shows the legend
 ax.legend()
 
 # exports graph in PNG format with 300 DPI
