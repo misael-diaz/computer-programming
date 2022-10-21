b3c89b3ef0fa4dd75a3ddbab8093f87dba3bdccc
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2022-10-21 10:18:54 -0400

    finds the convex hull with brute force [java]
    COMMENTS:
    Finds the 2D convex hull with brute force.
    
    The brute force algorithm rejects the hull if there is at least
    one interior angle of 180 degrees by throwing a runtime exception.
    
    The preliminary time complexity experiments show that the brute
    force algorithm executes on cubic time as expected.
    
    The Coordinate Class and the Vector Class have been borrowed
    (unaltered) from the source files used to solve the Closest Pair
    problem. The Time Class has been modified to catch the runtime
    exception thrown by the application if the hull gets rejected.
    The measured runtimes are unaffected by this feature.

---
+++ src/containers/vector/hull/java/ConvexHull.java
@@ -0,0 +1,456 @@
+/*
+ * Algorithms and Complexity                            October 19, 2022
+ * IST 4310
+ * Prof. M. Diaz-Maldonado
+ *
+ *
+ * Synopsis:
+ * Finds the Convex Hull from a set of 2D Cartesian coordinates.
+ *
+ *
+ * Copyright (c) 2022 Misael Diaz-Maldonado
+ * This file is released under the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ * References:
+ * [0] JJ McConnell, Analysis of Algorithms, 2nd edition, 2007
+ * [1] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
+ *
+ */
+
+import java.util.Random;
+
+public class ConvexHull
+{
+
+	/* elapsed-times (nanoseconds) */
+
+
+	public static double etimeBruteForce;
+	public static double etimeRecursive;
+
+
+	public static void main (String [] args)
+	{
+		testBruteForceMethod();
+		return;
+	}
+
+
+	/* implementations */
+
+
+	private static Vector genDataSet ()
+	// generates a data set of 2D Cartesian coordinates
+	{
+		/*
+
+		// initializes the dataset of problem 2.4.1-1
+		int [][] data = {
+			{ 1,  6}, { 4, 15}, { 7,  7}, {10, 13}, {11,  6},
+			{11, 18}, {11, 21}, {12, 10}, {15, 18}, {16,  6},
+			{18,  3}, {18, 12}, {19, 15}, {22, 19}
+		};
+		
+		*/
+
+
+		/*
+
+		// initializes the dataset of problem 2.4.1-2
+		int [][] data = {
+			{ 2, 20}, {3, 12}, { 5,  5}, { 6,  8}, {6, 17},
+			{ 8,  9}, {10, 2}, {10, 15}, {11,  7}, {11, 11},
+			{14, 14}, {16, 7}, {18, 15}, {19,  6}, {19, 10},
+			{22, 16}
+		};
+
+		*/
+
+
+		// initializes the dataset of problem 2.4.1-3
+		int [][] data = {
+			{ 2, 14}, { 4,  3}, { 5,  8}, { 5, 17}, { 6,  5},
+			{ 6, 21}, { 8, 11}, { 9, 17}, {10,  7}, {10, 19},
+			{12,  5}, {12, 14}, {16,  9}, {18, 12}, {18, 17},
+			{19,  4}, {19,  6}, {21, 15}, {22, 20}, {23,  9}
+		};
+
+
+		/*
+
+		// yields a Bad Convex Hull (complains as it should)
+		int [][] data = { {0, 0}, {2, 0}, {2, 2}, {1, 1} };
+
+		*/
+
+
+		// gets the shape of the datset
+		int rows = data.length, cols = data[0].length;
+		// gets the number of coordinates from the max dimension
+		int size = (rows > cols)? rows : cols;
+
+		Vector points = new Vector(size);
+		// stacks the dataset of coordinates on the vector
+		for (int i = 0; i != size; ++i)
+		{
+			int x = data[i][0];
+			int y = data[i][1];
+			Coord c = new Coord(x, y);
+			points.push_back(c);
+		}
+
+		return points;
+	}
+
+
+	private static boolean isEdge (Vector points, Coord P, Coord Q)
+	// returns true if the line PQ is an edge of the convex hull
+	{
+		int size = points.size();	// gets the data size
+		Line ln = new Line(P, Q);	// creates a line object
+
+		/*
+
+		traverses the whole vector until we can initialize the
+		sign with a non-zero value
+
+		*/
+
+		int k = 0;
+		int isign = 0;
+		while (isign == 0 && k != size)
+		{
+			Coord R = points.getData(k);
+			int sgn = ln.loc(R);
+			if (sgn != 0)
+				isign = sgn;
+			++k;
+		}
+
+
+		/*
+
+		Now that we have an initial sign we can check for sign
+		changes on the remainder of the vector.
+
+		*/
+
+
+		boolean isEdge = true;
+		for (int l = k; l != size; ++l)
+		{
+			Coord R = points.getData(l);
+			int sgn = ln.loc(R);
+			if (sgn * isign < 0)
+				isEdge = false;
+		}
+
+		return isEdge;
+	}
+
+
+	private static boolean isClosed (Vector vertices, Coord P, Coord Q)
+	// returns true if all the vertices have been found
+	{
+		int pos1 = vertices.search(P), pos2 = vertices.search(Q);
+		if (pos1 >= 0 && pos2 >= 0)
+			return true;
+		else
+			return false;
+	}
+
+
+	private static boolean addVertex(Vector vertices, Coord P, Coord Q)
+	/*
+
+	Synopsis:
+	Pushes unique vertices unto the back of the vector of vertices. As
+	a side effect, it sets the closed state of the convex hull. This
+	happens when both points P and Q are already vertices of the convex
+	hull. Generally this means that the algorithm has found all the
+	vertices. The other possibility is that we have a bad convex hull
+	with at least one internal angle equal to 180 degrees; this is the
+	main reason for setting the closed state of the convex hull.
+
+	*/
+	{
+		// checks if all the vertices have been found
+		boolean closed = isClosed(vertices, P, Q);
+		// pushes new vertices unto the back of the vector
+		if (vertices.search(P) < 0) vertices.push_back(P);
+		if (vertices.search(Q) < 0) vertices.push_back(Q);
+		// sorts vector to support underlying binary search method
+		vertices.sort();
+		// returns the closed state for the next pass
+		return closed;
+	}
+
+
+	private static void BadHull ()
+	// complains if the data set does not contain a convex hull
+	{
+		String errmsg = "BadHullError";
+		throw new RuntimeException(errmsg);
+	}
+
+
+	private static void BadHull (String errmsg)
+	// complains if the data set does not contain a convex hull
+	{
+		throw new RuntimeException(errmsg);
+	}
+
+
+	private static void isBadHull (boolean isClosed)
+	/*
+
+	Synopsis:
+	Complains if both vertices P and Q are already in the stack of
+	vertices. This means that there is at least a 180 degrees vertex.
+	Note that our acceptable range for the internal angle of a vertex
+	is [90, 180) degrees.
+
+	*/
+	{
+		if (isClosed)
+			BadHull("180AngleError");
+	}
+
+
+	private static void isBadHull (Vector points)
+	/*
+
+	Synopsis:
+	Uses brute force to determine if the data set of coordinates
+	does not contain a convex hull whose interior angles are in the
+	range [90, 180) --- a bad convex hull.
+
+	*/
+	{
+		// assumes that all the vertices have not been found
+		boolean closed = false;
+
+		// creates a placeholder for the vertices of the hull
+		Vector vertices = new Vector ();
+		
+		int size = points.size();
+		// considers all the possible edges, size * (size - 1) / 2
+		for (int i = 0; i != (size - 1); ++i)
+		{
+			Coord P = points.getData(i);
+			for (int j = (i + 1); j != size; ++j)
+			{
+
+				/*
+
+				creates the line PQ and checks if it is an
+				edge of the convex hull
+
+				*/
+
+				Coord Q = points.getData(j);
+				Line ln = new Line(P, Q);
+
+				/*
+
+				traverses the whole vector until we can
+				initialize the sign with a non-zero value
+
+				*/
+
+				int k = 0;
+				int isign = 0;
+				while (isign == 0 && k != size)
+				{
+					Coord R = points.getData(k);
+					int sgn = ln.loc(R);
+					if (sgn != 0)
+						isign = sgn;
+					++k;
+				}
+
+
+				/* complains if it is a bad hull (line) */
+
+
+				// complains if all the points form a line
+				if (isign == 0)
+					BadHull();
+
+
+				/*
+
+				Now that we have an initial sign we can
+				check for sign changes on the remainder
+				of the vector. Note: the points P and Q
+				form an edge if there are no sign changes.
+
+				*/
+
+
+				boolean isEdge = true;
+				for (int l = k; l != size; ++l)
+				{
+					Coord R = points.getData(l);
+					int sgn = ln.loc(R);
+					if (sgn * isign < 0)
+						isEdge = false;
+				}
+
+
+				if (isEdge)
+				// adds vertices if line PQ is a hull edge
+				{
+					isBadHull(closed);
+					closed = addVertex(vertices, P, Q);
+				}
+			}
+		}
+	}
+
+
+	private static Vector bruteforce (Vector points)
+	// returns the vertices of the convex hull in a vector
+	{
+		// creates the placeholder for the vertices of the hull
+		Vector vertices = new Vector ();
+		
+		int size = points.size();
+		// considers all the possible edges, size * (size - 1) / 2
+		for (int i = 0; i != (size - 1); ++i)
+		{
+			Coord P = points.getData(i);
+			for (int j = (i + 1); j != size; ++j)
+			// stacks points P and Q if they form a hull edge
+			{
+				Coord Q = points.getData(j);
+
+				if ( isEdge(points, P, Q) )
+					addVertex(vertices, P, Q);
+			}
+		}
+
+		return vertices;
+	}
+
+
+	/* static methods */
+
+
+	public static Vector genDataSet (int size)
+	/*
+
+	Synopsis:
+	Generates a distinct data set of coordinates by sampling values
+	from a uniform pseudo-random number generator PRNG.
+
+	Inputs:
+	size		size of the data set (number of particles)
+
+	Output:
+	vector		a vector that stores the data set of coordinates
+
+	*/
+	{
+		// creates a vector for storing the coordinates
+		Vector vector = new Vector();
+		// creates a pseudo-random number generator PRNG
+		//long seed = System.nanoTime();
+		Random random = new Random();
+		// defines limits for the particle coordinates
+		int x_min = -size, x_max = size;
+		int y_min = -size, y_max = size;
+		for (int i = 0; i != size; ++i)
+		// generates the distinct set of (x, y) coordinates
+		{
+			int x = x_min + random.nextInt(x_max - x_min);
+			int y = y_min + random.nextInt(y_max - y_min);
+			Coord c = new Coord (x, y);
+			while (vector.search(c) >= 0)
+			// generates a new coordinate if already in vector
+			{
+				x = random.nextInt(size);
+				y = random.nextInt(size);
+				c = new Coord (x, y);
+			}
+			// pushes (distinct) coordinate unto back of vector
+			vector.push_back(c);
+			// sorts to use binary search on next pass
+			vector.sort();
+		}
+
+		return vector;
+	}
+
+
+	public static Vector BruteForce (Vector points)
+	// uses brute force to obtain the convex hull
+	{
+		// complains if there is no convex hull
+		isBadHull(points);
+
+		/* times the implementation that finds the convex hull */
+
+		double start = System.nanoTime();
+		Vector vertices = bruteforce(points);
+		double end = System.nanoTime();
+
+		// computes the elapsed time in nanoseconds
+		etimeBruteForce = (end - start);
+
+		return vertices;
+	}
+
+
+	/* tests */
+
+
+	private static void testLineLocMethod ()
+	// tests if `Line.loc()' detects sign changes properly
+	{
+		int x1 = 0, y1 = 0;
+		int x2 = 4, y2 = 4;
+		int xi = -1, yi = -1;
+		Coord P = new Coord(x1, y1);
+		Coord Q = new Coord(x2, y2);
+		Coord R = new Coord(xi, yi);
+
+		Line line = new Line(P, Q);
+
+		System.out.printf("on the line (0): %d\n", line.loc(R));
+
+		xi = -1;
+		yi = -2;
+		Coord S = new Coord(xi, yi);
+		System.out.printf("below line (-1): %d\n", line.loc(S));
+		
+		xi = -2;
+		yi = -1;
+		Coord T = new Coord(xi, yi);
+		System.out.printf("above line (1): %d\n", line.loc(T));
+	}
+
+
+	private static void testBruteForceMethod ()
+	{
+		// creates the data set of Cartesian coordinates
+		Vector points = genDataSet();
+		// finds the vertices of the convex hull with brute force
+		Vector vertices = bruteforce (points);
+		// displays the coordinates of the vertices on the console
+		vertices.print();
+	}
+}
+
+
+/*
+
+COMMENTS:
+The code solves the convex hull problems of section 2.4.1 (Ref [0]) and
+detects if the convex hull has an interior angle of 180 degrees (bad).
+The Brute Force algorithm exhibits the expected cubic time complexity.
+
+*/
---
+++ src/containers/vector/hull/java/Vector.java
@@ -0,0 +1,528 @@
+/*
+ * Algorithms and Complexity                            October 06, 2022
+ * IST 4310
+ * Prof. M. Diaz-Maldonado
+ *
+ *
+ * Synopsis:
+ * Possible implementation of a vector of coordinates (x, y) in Java.
+ *
+ *
+ * Copyright (c) 2022 Misael Diaz-Maldonado
+ * This file is released under the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ * References:
+ * [0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
+ * [1] Sorting Custom Objects Tutorial: (
+ * 	www.codejava.net/java-core/collections/
+ * 	sorting-arrays-examples-with-comparable-and-comparator
+ * )
+ *
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Comparator;
+
+public class Vector
+{
+
+	/* defines comparator for y-x sorting of coordinates */
+
+
+	public static Comparator<Coord> comparator = (Coord P, Coord Q)->{
+		if ( P.getY() != Q.getY() )
+			return ( P.getY() - Q.getY() );
+		else
+			return ( P.getX() - Q.getX() );
+	};
+
+
+	/* initializes parameters (not bound to objects of the class) */
+
+
+	private static final int DEFAULT_SIZE_LIMIT = (0x00000010);
+
+
+	/* defines private data members */
+
+
+	private int begin;	// beginning of the placeholder (0)
+	private int avail;	// available location for writing
+	private int limit;	// size limit for storing data
+	private Coord [] data;	// data placeholder
+
+
+	/* constructors */
+
+
+	Vector ()
+	// default constructor: creates a vector of default size limit
+	{
+		this.begin = 0;
+		this.avail = 0;
+		this.limit = DEFAULT_SIZE_LIMIT;
+		this.data  = new Coord [limit];
+	}
+
+
+	Vector (int size)
+	// constructs a vector with requested capacity for storage
+	{
+		this.begin = 0;
+		this.avail = 0;
+		this.limit = size;
+		this.data  = new Coord [size];
+	}
+
+
+	Vector (Vector vector)
+	// copy constructor
+	{
+		this.begin = 0;			// sets to default
+		this.avail = vector.size();	// gets the size
+		this.limit = vector.size();	// fits to size
+		this.data  = vector.getData();	// gets a copy of the data
+	}
+
+
+	/* getters */
+
+
+	public Coord [] getData ()
+	// returns a clone of the data contained in vector
+	{
+		return Arrays.copyOfRange(data, begin, avail);
+	}
+
+
+	public Coord getData (int i)
+	// returns a copy of the ith element
+	{
+		return new Coord(this.data[i]);
+	}
+
+
+	/* methods */
+
+
+	public void clear ()
+	// effectively clears the vector elements
+	{
+		this.avail = 0;
+	}
+
+
+	public int size ()
+	// returns the number of elements stored in the vector
+	{
+		return (this.avail - this.begin);
+	}
+
+
+	public int capacity ()
+	// returns the storage capacity of the vector
+	{
+		return (this.limit - this.begin);
+	}
+
+
+	public void push_back (Coord x)
+	// pushes coordinates unto the back of vector
+	{
+		back_inserter (this, x);
+	}
+
+
+	public Vector bisect ()
+	// bisects vector, keeps first half and returns second half
+	{
+		/*
+
+		Computes the sizes of the first and second halves,
+		note that these differ when the (original) vector
+		stores an odd number of elements.
+
+		*/
+
+		int size = this.size();			// original size
+		int sizeHalf1 = (size / 2);		// first-half size
+		int sizeHalf2 = (size - sizeHalf1);	// second-half size
+
+		// creates the (fitted-to-size) returned vector
+		Vector ret = new Vector(sizeHalf2);
+		// copies data in second half into the returned vector
+		for (int i = 0; i != sizeHalf2; ++i)
+			ret.data[i] = new Coord(this.data[i + sizeHalf1]);
+
+		// effectively deletes second half from (original) vector
+		this.avail = sizeHalf1;
+		// sets the size of the returned vector
+		ret.avail  = sizeHalf2;
+		// returns the vector that contains the second half
+		return ret;
+	}
+
+
+	public void sort ()
+	// delegates the task of sorting to the sort method of Arrays
+	{
+		Arrays.sort (this.data, this.begin, this.avail);
+	}
+
+
+	public void sort (Comparator<Coord> comp)
+	// delegates the task of sorting to the sort method of Arrays
+	{
+		Arrays.sort (data, begin, avail, comp);
+	}
+
+
+	public int search (Coord key)
+	// delegates the task to the Binary Search method of Arrays
+	{
+		return Arrays.binarySearch (data, begin, avail, key);
+	}
+
+
+	public int search (Coord key, Comparator<Coord> comp)
+	// delegates the task to the Binary Search method of Arrays
+	{
+		return Arrays.binarySearch (data, begin, avail, key, comp);
+	}
+
+
+	public void print ()
+	// prints the (x, y) coordinates on the console
+	{
+		int size = this.size();
+		for (int i = 0; i != size; ++i)
+		{
+			Coord c = this.data[i];
+			String fmt = ("x, y = (%8d, %8d)\n");
+			System.out.printf(fmt, c.getX(), c.getY());
+		}
+	}
+
+
+	/* implementations */
+
+
+	private void back_inserter (Vector v, Coord x)
+	// pushes data into the back of vector
+	{
+		int avail = v.avail;	// gets vector size
+		int limit = v.limit;	// gets vector size limit
+
+		if (avail == limit)	// checks if there's space left
+			grow (v);	// doubles the vector size limit
+
+		v.data[avail] = x;	// writes at available location
+		++v.avail;		// increments vector size
+	}
+
+
+	private void grow (Vector v)
+	// doubles the vector size
+	{
+		int limit = v.limit;		// gets current size limit
+		Coord [] tmp = v.data.clone();	// copies data to temporary
+
+		v.limit *= 2;			// doubles size limit
+		v.data = new Coord[v.limit];	// doubles allocation
+		for (int i = 0; i != limit; ++i)// restores data
+			v.data[i] = tmp[i];
+	}
+
+
+
+	public static void main (String[] args)
+	// tests the methods of the vector class
+	{
+		testPushBackMethod();
+		testClearMethod();
+		testSortMethod();
+		testSearchMethod();
+		return;
+	}
+
+
+	/* tests */
+
+
+	private static void testPushBackMethod ()
+	/*
+
+	Synopsis:
+	Pushes coordinates unto the back of the vector and checks its size.
+
+	*/
+	{
+		int size = 32;
+		Vector vector = new Vector ();
+		for (int i = 0; i != size; ++i)
+			vector.push_back ( new Coord(i, i) );
+
+		System.out.printf("push-back-method-test: ");
+		// checks the vector size against the expected size
+		if (vector.size() != size)
+			System.out.println("FAIL");
+		else
+			System.out.println("pass");
+	}
+
+
+	private static void testClearMethod ()
+	/*
+
+	Synopsis:
+	Pushes coordinates unto the back of the vector, clears it, and
+	checks that it is empty.
+
+	*/
+	{
+		int size = 32;
+		Vector vector = new Vector ();
+		for (int i = 0; i != size; ++i)
+			vector.push_back ( new Coord(i, i) );
+
+
+		vector.clear ();	// clears vector
+
+
+		System.out.printf("clear-method-test: ");
+		// checks the vector size against the expected size
+		if (vector.size() != 0)
+			System.out.println("FAIL");
+		else
+			System.out.println("pass");
+	}
+
+
+	private static void testSortMethod ()
+	/*
+
+	Synopsis:
+	Pushes random coordinates unto the back of the vector, sorts it
+	in ascending order, and checks if the sorting was successful.
+
+	*/
+	{
+		int size = 8;
+		Vector vector = new Vector (size);
+
+		/*
+
+		NullPointerException Test: Sorts Empty Vector
+
+		Passes test if no exception is thrown when executed.
+
+		Note that the exception should not occur because the sort
+		method of the vector class specifies the sorting range.
+
+		*/
+
+		vector.sort();
+
+
+		/* Pushes data unto back vector */
+
+
+		Random random = new Random ();
+		for (int i = 0; i != size; ++i)
+		// fills vector with random coordinates
+		{
+			int x = random.nextInt (size);
+			int y = random.nextInt (size);
+			Coord coord = new Coord (x, y);
+			vector.push_back (coord);
+		}
+
+
+		vector.sort();	// sorts data contained in vector
+
+
+		Coord [] data = vector.getData();
+		for (int i = 0; i != size; ++i)
+		// prints the (sorted) coordinates on the console
+		{
+			int x = data[i].getX();
+			int y = data[i].getY();
+			System.out.printf("x: %2d y: %2d\n", x, y);
+		}
+
+
+		int failures = 0;
+		for (int i = 0; i != (size - 1); ++i)
+		// counts failures (not in ascending order instances)
+		{
+			Coord thisCoord = data[i], nextCoord = data[i + 1];
+			if (thisCoord.compareTo(nextCoord) > 0)
+				++failures;
+		}
+
+
+		System.out.printf("sort-method-test[0]: ");
+		// checks if the sorting method failed (unexpectedly)
+		if (failures != 0)
+			System.out.println("FAIL");
+		else
+			System.out.println("pass");
+
+
+		/* performs y - x sorting */
+
+
+		vector.sort(comparator);
+		data = vector.getData();
+		System.out.println("y-x sorting:");
+		for (int i = 0; i != size; ++i)
+		// prints the (sorted) coordinates on the console
+		{
+			int x = data[i].getX();
+			int y = data[i].getY();
+			System.out.printf("x: %2d y: %2d\n", x, y);
+		}
+
+
+		failures = 0;
+		for (int i = 0; i != (size - 1); ++i)
+		// counts failures (not in ascending order instances)
+		{
+			Coord thisCoord = data[i], nextCoord = data[i + 1];
+			if (comparator.compare(thisCoord, nextCoord) > 0)
+				++failures;
+		}
+
+
+		System.out.printf("sort-method-test[1]: ");
+		// checks if the sorting method failed (unexpectedly)
+		if (failures != 0)
+			System.out.println("FAIL\n");
+		else
+			System.out.println("pass\n");
+	}
+
+
+	private static void testSearchMethod ()
+	/*
+
+	Synopsis:
+	Uses search method to create a distinct set of (x, y) coordinates.
+
+	*/
+	{
+		Vector vector = new Vector ();	// creates (empty) vector
+		Random random = new Random ();	// creates (default) PRNG
+
+
+		/* creates data set from random data */
+
+
+		int size = (0x00000010);
+		for (int i = 0; i != size; ++i)
+		// generates the distinct set of (x, y) coordinates
+		{
+			int x = random.nextInt(size);
+			int y = random.nextInt(size);
+			Coord c = new Coord (x, y);
+			while (vector.search(c) >= 0)
+			// generates a new coordinate if already in vector
+			{
+				x = random.nextInt(size);
+				y = random.nextInt(size);
+				c = new Coord (x, y);
+			}
+			// pushes (distinct) coordinate unto back of vector
+			vector.push_back(c);
+			// sorts to use binary search on next pass
+			vector.sort();
+		}
+
+
+		/* Displays Data on the Console */
+
+
+		Coord [] data = vector.getData();
+		for (int i = 0; i != size; ++i)
+		// prints the (distinct set of) coordinates on the console
+		{
+			int x = data[i].getX();
+			int y = data[i].getY();
+			System.out.printf("x: %2d y: %2d\n", x, y);
+		}
+
+
+		/* Duplicates Test */
+
+
+		int duplicates = 0;
+		for (int i = 0; i != (size - 1); ++i)
+		// counts duplicates by checking for equality
+		{
+			Coord thisCoord = data[i], nextCoord = data[i + 1];
+			if (thisCoord.compareTo(nextCoord) == 0)
+				++duplicates;
+		}
+
+		System.out.printf("search-method-test[0]: ");
+		if (duplicates != 0)
+			System.out.println("FAIL");
+		else
+			System.out.println("pass");
+
+
+		/*
+
+		Performs y - x sorting test:
+
+		Test consists on searching the x-y sorted data after the
+		vector has been y-x sorted. We increment the number of
+		failures every time the method fails to find an element.
+
+		Note that if the comparator is not supplied to the binary
+		search method, it will fail sometimes because the data has
+		been y-x sorted while the method assumes x-y sorting.
+		On the other hand, when the comparator is supplied the
+		search is successful.
+
+		*/
+
+
+		int failures = 0;		// initializes counter
+		data = vector.getData();	// gets x - y sorted data
+		vector.sort(comparator);	// does y - x sorting
+		for (int i = 0; i != size; ++i)
+		// searches the x-y sorted data in the y-x sorted vector
+		{
+			Coord c = data[i];
+			if (vector.search(c, comparator) < 0)
+				++failures;
+		}
+
+
+		System.out.printf("search-method-test[1]: ");
+		/*
+
+		the number of failures should be zero because we are
+		searching for data that we know is contained in the vector
+
+		*/
+		if (failures != 0)
+			System.out.println("FAIL");
+		else
+			System.out.println("pass");
+	}
+}
+
+/*
+ * TODO:
+ * [x] overload the search method which accepts the y - x comparator as its
+ *     input argument. Note that the binary search algorithm will assume a
+ *     x - y ordering by default and because of that it can fail sometimes
+ *     if the comparator is not given.
+ *
+ */
