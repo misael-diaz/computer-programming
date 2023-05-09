c39c398edfbfc8fee3990dedf2c40e4f059be1ad
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-09 13:18:22 -0400

    changes indentation [Java]

--- src/functions/numeric/java/linspace.java
+++ src/functions/numeric/java/linspace.java
@@ -26,59 +26,53 @@
 
 class Numerics
 {
+  public static void main (String[] args)
+  {
+    System.out.println("\nFunction Example:\n");
 
-	public static void main (String[] args)
-	{
+    double xi = 0;				// initial array value
+    double xf = 7;				// final array value
+    int numel = 8;				// number of elements in the array
+    double x[] = linspace (xi, xf, numel);
 
-		System.out.println("\nFunction Example:\n");
-
-		double xi = 0;	// initial array value
-		double xf = 7;	// final array value
-		int numel = 8;	// number of elements in the array
-		double x[] = linspace (xi, xf, numel);
-
-		// displays the elements of the array on the console
-		System.out.println("prints array x = 0, 1, 2, ..., 6, 7:");
-		for (int i = 0; i != numel; ++i)
-			System.out.printf("%.6f\n", x[i]);
-
-	}
+    // displays the elements of the array on the console
+    System.out.println("prints array x = 0, 1, 2, ..., 6, 7:");
+    for (int i = 0; i != numel; ++i)
+      System.out.printf("%.6f\n", x[i]);
+  }
 
 
-	public static double[] linspace (double xi, double xf, int numel)
-	{
-		/*
-		 * Synopsis:
-		 * Implements the linspace function.
-		 *
-		 * Inputs:
-		 * xi		initial value of the array
-		 * xf		final value of the array
-		 * numel	number of elements of the array
-		 *
-		 * Output:
-		 * x		array [xi, xf] with equally spaced elements
-		 *
-		 */
+  // double[] linspace (double xi, double xf, int numel)
+  //
+  // Synopsis:
+  // Implements the linspace function.
+  //
+  // Inputs:
+  // xi		initial value of the array
+  // xf		final value of the array
+  // numel	number of elements of the array
+  //
+  // Output:
+  // x		array [xi, xf] with equally spaced elements
 
-		// caters invalid number of elements
-		if (numel <= 1)
-		{
-			throw new RuntimeException(
-				"number of elements must be greater " +
-				"than one"
-			);
-		}
 
-		double dx  = (xf - xi) / ( (double) (numel - 1) );
-		double x[] = new double [numel];
+  public static double[] linspace (double xi, double xf, int numel)
+  {
 
-		x[0] = xi;
-		for (int i = 1; i != numel; ++i)
-			x[i] = x[i-1] + dx;
+    if (numel <= 1)	// caters invalid number of elements
+    {
+      throw new RuntimeException("number of elements must be greater than one");
+    }
 
-		return x;
-	}
+    double dx  = (xf - xi) / ( (double) (numel - 1) );
+    double x[] = new double [numel];
+
+    x[0] = xi;
+    for (int i = 1; i != numel; ++i)
+      x[i] = x[i-1] + dx;
+
+    return x;
+  }
 }
 
 
