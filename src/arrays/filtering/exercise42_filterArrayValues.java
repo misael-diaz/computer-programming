3fceed5e1912742551651b4ba5add824a3125f9c
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-10 15:24:47 -0400

    changes indentation [Java]

--- src/arrays/filtering/exercise42_filterArrayValues.java
+++ src/arrays/filtering/exercise42_filterArrayValues.java
@@ -27,189 +27,186 @@ import java.util.Arrays;	// for filtering numbers
 
 public class exercise42_filterArrayValues
 {
-	public static void main (String[] args)
-	// filters out odd (or prunes) odd numbers from an array of pseudo-random numbers
+  // filters out odd (or prunes) odd numbers from an array of pseudo-random numbers
+  public static void main (String[] args)
+  {
+    int n = 16;			// sets the size of the array `numbers'
+    int [] numbers = create(n);	// gets `n' random numbers into array
+    example1(numbers);		// uses core language features
+    example2(numbers);		// uses the ArrayList utility
+    example3(numbers);		// uses the Arrays::stream::filter utility
+  }
+
+
+  // possible implementation via core language features
+  private static void example1 (int [] numbers)
+  {
+    // finds the number of even values contained in the array `numbers'
+    int numEvenValues = numEvenValues(numbers);
+
+    // caters odd-valued arrays (to avert memory allocation errors)
+    if (numEvenValues != 0)
+    {
+      int count = 0;
+      // Note: it is invalid to allocate an array of zero elements
+      int [] evenValues = new int [numEvenValues];
+      // copies the even values into the array temporary `evenValues'
+      for (int i = 0; i != numbers.length; ++i)
+      {
+	int number = numbers[i];
+	if (number % 2 == 0)
 	{
-		int n = 16;			// sets the size of the array `numbers'
-		int [] numbers = create(n);	// gets `n' random numbers into array
-		example1(numbers);		// uses core language features
-		example2(numbers);		// uses the ArrayList utility
-		example3(numbers);		// uses the Arrays::stream::filter utility
+	  evenValues[count] = number;
+	  ++count;
 	}
+      }
 
 
-	private static void example1 (int [] numbers)
-	// possible implementation via core language features
+      // post-processing:
+
+
+      System.out.print("even: ");
+      // displays on the console the even values
+      for (int i = 0; i != count; ++i)
+      {
+	int evenValue = evenValues[i];
+	if (i == 0)
 	{
-		// finds the number of even values contained in the array `numbers'
-		int numEvenValues = numEvenValues(numbers);
-
-		// caters odd-valued arrays (to avert memory allocation errors)
-		if (numEvenValues != 0)
-		{
-			int count = 0;
-			// Note: it is invalid to allocate an array of zero elements
-			int [] evenValues = new int [numEvenValues];
-			for (int i = 0; i != numbers.length; ++i)
-			// copies the even values into the array temporary `evenValues'
-			{
-				int number = numbers[i];
-				if (number % 2 == 0)
-				{
-					evenValues[count] = number;
-					++count;
-				}
-			}
-
-
-			// post-processing:
-
-
-			System.out.print("even: ");
-			// displays on the console the even values
-			for (int i = 0; i != count; ++i)
-			{
-				int evenValue = evenValues[i];
-				if (i == 0)
-				{
-					System.out.printf("%d", evenValue);
-				}
-				else
-				{
-					System.out.printf(", %d", evenValue);
-				}
-			}
-
-			System.out.println();
-		}
-		else
-		// otherwise, informs the user that the passed array has no even values
-		{
-			System.out.println("even: none");
-		}
+	  System.out.printf("%d", evenValue);
 	}
-
-
-	private static void example2 (int [] numbers)
-	// uses an ArrayList to insert dynamically the even values in `numbers'
+	else
 	{
-
-		// creates an empty ArrayList for storing the even values in `numbers'
-		ArrayList<Integer> evenValues = new ArrayList<>();
-
-		for (int i = 0; i != numbers.length; ++i)
-		// inserts dynamically the even values into the ArrayList `evenValues'
-		{
-			int number = numbers[i];
-			if (number % 2 == 0)
-			{
-				evenValues.add(number);
-			}
-		}
-
-
-		// post-processing:
-
-
-		// gets the number of even values from the size of the ArrayList
-		int count = evenValues.size();
-		if (count != 0)
-		{
-			System.out.print("even: ");
-			// displays on the console the even values
-			for (int i = 0; i != count; ++i)
-			{
-				// gets the ith element in the ArrayList `evenValues'
-				int evenValue = evenValues.get(i);
-				if (i == 0)
-				{
-					System.out.printf("%d", evenValue);
-				}
-				else
-				{
-					System.out.printf(", %d", evenValue);
-				}
-			}
-
-			System.out.println();
-		}
-		else
-		// otherwise, informs the user that the passed array has no even values
-		{
-			System.out.println("even: none");
-		}
+	  System.out.printf(", %d", evenValue);
 	}
+      }
+      System.out.println();
+    }
+    else
+    {
+      // otherwise, informs the user that the passed array has no even values
+      System.out.println("even: none");
+    }
+  }
 
 
-	private static void example3 (int [] nums)
-	// prunes the odd values from the array `nums' via Arrays::stream::filter()
+  // uses an ArrayList to insert dynamically the even values in `numbers'
+  private static void example2 (int [] numbers)
+  {
+
+    // creates an empty ArrayList for storing the even values in `numbers'
+    ArrayList<Integer> evenValues = new ArrayList<>();
+
+    // inserts dynamically the even values into the ArrayList `evenValues'
+    for (int i = 0; i != numbers.length; ++i)
+    {
+      int number = numbers[i];
+      if (number % 2 == 0)
+      {
+	evenValues.add(number);
+      }
+    }
+
+
+    // post-processing:
+
+
+    // gets the number of even values from the size of the ArrayList
+    int count = evenValues.size();
+    if (count != 0)
+    {
+      System.out.print("even: ");
+      // displays on the console the even values
+      for (int i = 0; i != count; ++i)
+      {
+	// gets the ith element in the ArrayList `evenValues'
+	int evenValue = evenValues.get(i);
+	if (i == 0)
 	{
-
-		// filters the stream according to the predicate lambda function (ref [0])
-		int [] even = Arrays.stream(nums).filter( e -> (e % 2 == 0) ).toArray();
-
-
-		// post-processing:
-
-
-		// gets the number of even values from the length of the array `even'
-		int count = even.length;
-		if (count != 0)
-		{
-			System.out.print("even: ");
-			// displays on the console the even values
-			for (int i = 0; i != count; ++i)
-			{
-				int e = even[i];
-				if (i == 0)
-				{
-					System.out.printf("%d", e);
-				}
-				else
-				{
-					System.out.printf(", %d", e);
-				}
-			}
-
-			System.out.println();
-		}
-		else
-		// otherwise, informs the user that the passed array has no even values
-		{
-			System.out.println("even: none");
-		}
+	  System.out.printf("%d", evenValue);
 	}
-
-
-	private static int numEvenValues (int [] numbers)
-	// counts the number of even values in the array
+	else
 	{
-		int count = 0;
-		for (int i = 0; i != numbers.length; ++i)
-		// invariant: we have counted `count' even elements in the array
-		{
-			int number = numbers[i];
-			if (number % 2 == 0)
-				count += 1;
-			else
-				count += 0;
-		}
-
-		return count;
+	  System.out.printf(", %d", evenValue);
 	}
+      }
+      System.out.println();
+    }
+    else
+    {
+      // otherwise, informs the user that the passed array has no even values
+      System.out.println("even: none");
+    }
+  }
 
 
-	private static int [] create (int numel)
-	// returns an array of pseudo-random numbers of `numel' elements with values
-	// in the asymmetric range [0, numel)
+  // prunes the odd values from the array `nums' via Arrays::stream::filter()
+  private static void example3 (int [] nums)
+  {
+
+    // filters the stream according to the predicate lambda function (ref [0])
+    int [] even = Arrays.stream(nums).filter( e -> (e % 2 == 0) ).toArray();
+
+
+    // post-processing:
+
+
+    // gets the number of even values from the length of the array `even'
+    int count = even.length;
+    if (count != 0)
+    {
+      System.out.print("even: ");
+      // displays on the console the even values
+      for (int i = 0; i != count; ++i)
+      {
+	int e = even[i];
+	if (i == 0)
 	{
-		Random r = new Random();
-		int [] numbers = new int[numel];
-		for (int i = 0; i != numel; ++i)
-		{
-			numbers[i] = r.nextInt(numel);
-		}
-
-		return numbers;
+	  System.out.printf("%d", e);
 	}
+	else
+	{
+	  System.out.printf(", %d", e);
+	}
+      }
+      System.out.println();
+    }
+    else
+    {
+      // otherwise, informs the user that the passed array has no even values
+      System.out.println("even: none");
+    }
+  }
+
+
+  // counts the number of even values in the array
+  private static int numEvenValues (int [] numbers)
+  {
+    int count = 0;
+    // invariant: we have counted `count' even elements in the array
+    for (int i = 0; i != numbers.length; ++i)
+    {
+      int number = numbers[i];
+      if (number % 2 == 0)
+	count += 1;
+      else
+	count += 0;
+    }
+
+    return count;
+  }
+
+
+  private static int [] create (int numel)
+  {
+    // returns an array of pseudo-random numbers of `numel' elements with values
+    // in the asymmetric range [0, numel)
+    Random r = new Random();
+    int [] numbers = new int[numel];
+    for (int i = 0; i != numel; ++i)
+    {
+      numbers[i] = r.nextInt(numel);
+    }
+
+    return numbers;
+  }
 }
