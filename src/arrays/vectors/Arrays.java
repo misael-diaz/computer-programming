b0fd6c3585da644ea129ecf24796dc67231dabc9
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-10 15:15:03 -0400

    changes indentation [Java]

--- src/arrays/vectors/Arrays.java
+++ src/arrays/vectors/Arrays.java
@@ -23,92 +23,79 @@
 public class Arrays
 {
 
-	public static void main (String[] args)
-	{
+  public static void main (String[] args)
+  {
 
-		System.out.println("\nArray Examples:\n");
-
-
-
-		/* example 1.6-C: */
-		/* y = f(x) = 0.4 * x^4 + 3.1 * x^2 - 162.3 * x - 80.7 */
-		int size = 8;			// defines the array size
-		double dx  = 1.0;		// sets spacing or step
-		double x[] = new double [size];	// creates array of doubles
-		double y[] = new double [size];	// y = f(x)
-
-
-		x[0] = 0.0;
-		/* assigns values to the array elements */
-		for (int i = 0; i != size; ++i)
-			x[i] = x[0] + ( (float) i ) * dx;
+    System.out.println("\nArray Examples:\n");
 
 
 
-		/* calculates the elements of the array y = f(x) */
-		for (int i = 0; i != size; ++i)
-		{
-			y[i] = 0.4 * Math.pow(x[i], 4) +
-			       3.1 * Math.pow(x[i], 2) - 162.3 * x[i] -
-			       80.7;
-		}
+    // example 1.6-C:
+    // y = f(x) = 0.4 * x^4 + 3.1 * x^2 - 162.3 * x - 80.7
+    int size = 8;			// defines the array size
+    double dx  = 1.0;			// sets spacing or step
+    double x[] = new double [size];	// creates array of doubles
+    double y[] = new double [size];	// y = f(x)
 
 
-		System.out.println("Example 1.6-C:");
-		/* performs element addressing */
-		System.out.printf(
-			"references the 4th element of x: %12.6f\n", x[3]
-		);
-		System.out.printf(
-			"references the 4th element of y: %12.6f\n", y[3]
-		);
-		System.out.println("");
+    x[0] = 0.0;
+    // assigns values to the array elements
+    for (int i = 0; i != size; ++i)
+      x[i] = x[0] + ( (double) i ) * dx;
 
 
 
-		/* example 1.6-D: */
-		/* y = f(x) = 0.4 * x^4 + 3.1 * x^2 - 162.3 * x - 80.7 */
-		size = 8;	// uses the same array size as above
-		dx   = 2.0;	// sets spacing or step
+    // calculates the elements of the array y = f(x)
+    for (int i = 0; i != size; ++i)
+    {
+      y[i] = 0.4 * Math.pow(x[i], 4) + 3.1 * Math.pow(x[i], 2) - 162.3 * x[i] - 80.7;
+    }
 
 
-		x[0] = 0.0;
-		/* assigns values to the array elements */
-		for (int i = 0; i != size; ++i)
-			x[i] = x[0] + ( (float) i ) * dx;
+    System.out.println("Example 1.6-C:");
+    // performs element addressing
+    System.out.printf("references the 4th element of x: %12.6f\n", x[3]);
+    System.out.printf("references the 4th element of y: %12.6f\n", y[3]);
+    System.out.println("");
 
 
 
-		/* calculates the elements of the array y = f(x) */
-		for (int i = 0; i != size; ++i)
-		{
-			y[i] = 0.4 * Math.pow(x[i], 4) +
-			       3.1 * Math.pow(x[i], 2) - 162.3 * x[i] -
-			       80.7;
-		}
+    // example 1.6-D:
+    // y = f(x) = 0.4 * x^4 + 3.1 * x^2 - 162.3 * x - 80.7
+    size = 8;		// uses the same array size as above
+    dx   = 2.0;		// sets spacing or step
 
 
-		System.out.println("Example 1.6-D:");
-		/* performs element addressing */
-		System.out.printf(
-			"references the 7th element of x: %12.6f\n", x[6]
-		);
-		System.out.printf(
-			"references the 7th element of y: %12.6f\n", y[6]
-		);
-		System.out.println("");
+    x[0] = 0.0;
+    // assigns values to the array elements
+    for (int i = 0; i != size; ++i)
+      x[i] = x[0] + ( (double) i ) * dx;
 
 
-		// tabulates results in Pandas style
-		for (int i = 0; i != size; ++i)
-		{
-			String fstr = ("%2d %5.1f %8.1f\n"); 	// format-string
-			System.out.printf(fstr, i, x[i], y[i]);
-		}
 
-	}
+    // calculates the elements of the array y = f(x)
+    for (int i = 0; i != size; ++i)
+    {
+      y[i] = 0.4 * Math.pow(x[i], 4) + 3.1 * Math.pow(x[i], 2) - 162.3 * x[i] - 80.7;
+    }
+
+
+    System.out.println("Example 1.6-D:");
+    // performs element addressing
+    System.out.printf("references the 7th element of x: %12.6f\n", x[6]);
+    System.out.printf("references the 7th element of y: %12.6f\n", y[6]);
+    System.out.println("");
+
+
+    // tabulates results in Pandas style
+    for (int i = 0; i != size; ++i)
+    {
+      String fstr = ("%2d %5.1f %8.1f\n"); 	// format-string
+      System.out.printf(fstr, i, x[i], y[i]);
+    }
+
+  }
 }
-
 
 
 /*
