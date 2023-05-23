b30e84e54e76e45e878167601ebaff0462c32f77
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-23 10:20:00 -0400

    changes indentation and comment style [Java]

--- src/containers/strings/exercise39_numCharInstances.java
+++ src/containers/strings/exercise39_numCharInstances.java
@@ -27,162 +27,156 @@ import java.util.Scanner;
 
 public class exercise39_numCharInstances
 {
-	public static void main (String [] args)
+  public static void main (String [] args)
+  {
+    // creates readline `rl' object for storing a line of input into a string
+    Scanner rl = new Scanner(System.in);
+
+    // prompts users for a message and stores it in a string
+    System.out.print("input a message> ");
+    String msg = rl.nextLine();
+
+    // displays on the console the frequency of each character in the message
+    frequency(msg);
+  }
+
+
+  // implementations:
+
+
+  // displays on the console the number of character instances in the message `msg'
+  private static void frequency (String msg)
+  {
+
+    // pre-processing:
+
+
+    // removes whitespace from message `msg'
+    String str = removeWhitespace(msg);
+
+
+    // obtains the frequency of characters in the string `str'
+    int [] freq = distinct(str);
+
+
+    // post-processing:
+
+
+    // loop-invariant:
+    // we have reported on the console the number of instances of `count'
+    // different characters present in the string `str'
+
+
+    int count = 0;
+    // displays on the console the frequency of characters in the message
+    for (int i = 0; i != str.length(); ++i)
+    {
+      // checks the partition array `freq' for the number of instances
+      // of the character at the ith position in the string
+      if (freq[i] < 0)
+      {
+	if (count == 0)	// checks if displaying on the console for the first time
 	{
-		// creates readline `rl' object for storing a line of input into a string
-		Scanner rl = new Scanner(System.in);
-
-		// prompts users for a message and stores it in a string
-		System.out.print("input a message> ");
-		String msg = rl.nextLine();
-
-		// displays on the console the frequency of each character in the message
-		frequency(msg);
+	  String fstr = "%c: %d";				// format string
+	  System.out.printf(fstr, str.charAt(i), -freq[i]);
+	  ++count;
 	}
-
-
-	// implementations:
-
-
-	private static void frequency (String msg)
-	// displays on the console the number of character instances in the message `msg'
+	else		// if not the first time, insert the delimiter `, '
 	{
-
-		// pre-processing:
-
-
-		// removes whitespace from message `msg'
-		String str = removeWhitespace(msg);
-
-
-		// obtains the frequency of characters in the string `str'
-		int [] freq = distinct(str);
-
-
-		// post-processing:
-
-
-		// loop-invariant:
-		// we have reported on the console the number of instances of `count'
-		// different characters present in the string `str'
-
-
-		int count = 0;
-		// displays on the console the frequency of characters in the message
-		for (int i = 0; i != str.length(); ++i)
-		{
-			// checks the partition array `freq' for the number of instances
-			// of the character at the ith position in the string
-			if (freq[i] < 0)
-			{
-				// checks if displaying on the console for the first time
-				if (count == 0)
-				{
-					String fstr = "%c: %d";		// format string
-					System.out.printf(fstr, str.charAt(i), -freq[i]);
-					++count;
-				}
-				else
-				// if not the first time, insert the delimiter `, '
-				{
-					String dlim = ", ";		// delimiter
-					String elem = "%c: %d";		// element
-					String fstr = dlim + elem;	// format string
-					System.out.printf(fstr, str.charAt(i), -freq[i]);
-					++count;
-				}
-			}
-		}
-
-		System.out.println();
+	  String dlim = ", ";					// delimiter
+	  String elem = "%c: %d";				// element
+	  String fstr = dlim + elem;				// format string
+	  System.out.printf(fstr, str.charAt(i), -freq[i]);
+	  ++count;
 	}
+      }
+    }
+
+    System.out.println();
+  }
 
 
-	private static boolean contains (String str, char target)
-	/*
+  // boolean contains (String str, char target)
+  //
+  // Synopsis:
+  // Returns true if the target character `target' is in the string `str', returns
+  // false otherwise.
 
-	Synopsis:
-	Returns true if the target character `target' is in the string `str', returns
-	false otherwise.
 
-	*/
+  private static boolean contains (String str, char target)
+  {
+    int pos = search(str, target);
+    if (pos < 0)
+      return false;
+    else
+      return true;
+  }
+
+
+  // int search (String str, char target)
+  //
+  // Synopsis:
+  // Searches linearly for the target character `target' in the string `str' and
+  // returns its positional index if its present, otherwise returns an invalid index.
+
+
+  private static int search (String str, char target)
+  {
+    int pos = -1;
+    for (int i = 0; i != str.length(); ++i)
+    {
+      if (str.charAt(i) == target)
+      {
+	pos = i;
+	return pos;
+      }
+    }
+
+    return pos;
+  }
+
+
+  // returns a string containing the distinct characters in `msg' string
+  private static int [] distinct (String msg)
+  {
+
+    int [] part = new int[msg.length()];
+    for (int i = 0; i != msg.length(); ++i)
+    {
+      part[i] = -1;	// initializes the partition array for finding duplicates
+    }
+
+
+    // uses brute force to find the distinct characters in the message
+    for (int i = 0; i != (msg.length() - 1); ++i)
+    {
+      for (int j = (i + 1); j != msg.length(); ++j)
+      {
+	// if not marked as duplicate and if repeated, then:
+	if ( (part[j] < 0) && ( msg.charAt(i) == msg.charAt(j) ) )
 	{
-		int pos = search(str, target);
-		if (pos < 0)
-			return false;
-		else
-			return true;
+	  part[j] = i;		// sets the ith char as the parent of the jth char
+	  --part[i];		// increments number of ith character duplicates
 	}
+      }
+    }
+
+    return part;
+  }
 
 
-	private static int search (String str, char target)
-	/*
+  // returns a copy of the string `msg' without whitespace ' '
+  private static String removeWhitespace (String msg)
+  {
+    String str = new String();
+    for (int i = 0; i != msg.length(); ++i)
+    {
+      if (msg.charAt(i) != ' ')
+      {
+	str += msg.charAt(i);
+      }
+    }
 
-	Synopsis:
-	Searches linearly for the target character `target' in the string `str' and
-	returns its positional index if its present, otherwise returns an invalid
-	index.
-
-	*/
-	{
-		int pos = -1;
-		for (int i = 0; i != str.length(); ++i)
-		{
-			if (str.charAt(i) == target)
-			{
-				pos = i;
-				return pos;
-			}
-		}
-
-		return pos;
-	}
-
-
-	private static int [] distinct (String msg)
-	// returns a string containing the distinct characters in `msg' string
-	{
-
-		int [] part = new int[msg.length()];
-		for (int i = 0; i != msg.length(); ++i)
-		// initializes the partition array for finding duplicates
-		{
-			part[i] = -1;
-		}
-
-
-		// uses brute force to find the distinct characters in the message
-		for (int i = 0; i != (msg.length() - 1); ++i)
-		{
-			for (int j = (i + 1); j != msg.length(); ++j)
-			{
-				// if not marked as duplicate and if repeated, then:
-				if ( (part[j] < 0) && ( msg.charAt(i) == msg.charAt(j) ) )
-				{
-					// sets the ith char as the parent of the jth char
-					part[j] = i;
-					// increments number of ith character duplicates
-					--part[i];
-				}
-			}
-		}
-
-		return part;
-	}
-
-
-	private static String removeWhitespace (String msg)
-	// returns a copy of the string `msg' without whitespace ' '
-	{
-		String str = new String();
-		for (int i = 0; i != msg.length(); ++i)
-		{
-			if (msg.charAt(i) != ' ')
-			{
-				str += msg.charAt(i);
-			}
-		}
-
-		return str;
-	}
+    return str;
+  }
 }
