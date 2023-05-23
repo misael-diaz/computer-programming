cfa172ebbe4277bf2622d81ac6d360848d413759
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-23 13:57:51 -0400

    fixes URLs [Java]
    COMMENTS:
    So that the next programmer can consult the references easily.

--- src/io/java/Files.java
+++ src/io/java/Files.java
@@ -15,17 +15,14 @@
  *
  *
  * References:
- * [0] Files: www.w3schools.com/java/java_files_create.asp
- * [1] PrintWriter: (docs.oracle.com/en/java/javase/11/docs/api/java.base/
- *                   java/io/PrintWriter.html)
- * [2] IOException: (docs.oracle.com/javase/7/docs/api/java/io/
- *                   IOException.html)
- * [3] FileNotFoundException: (docs.oracle.com/javase/7/docs/api/java/io/
- *                             FileNotFoundException.html)
- * [4] Scanner: docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
- * [5] Random: docs.oracle.com/javase/8/docs/api/java/util/Random.html
- * [6] www.javatpoint.com/throw-keyword
- * [7] www.javatpoint.com/post/java-random
+ * [0] https://www.w3schools.com/java/java_files_create.asp
+ * [1] https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/PrintWriter.html
+ * [2] https://docs.oracle.com/javase/7/docs/api/java/io/IOException.html
+ * [3] https://docs.oracle.com/javase/7/docs/api/java/io/FileNotFoundException.html
+ * [4] https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+ * [5] https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
+ * [6] https://www.javatpoint.com/throw-keyword
+ * [7] https://www.javatpoint.com/post/java-random
  *
  */
 
