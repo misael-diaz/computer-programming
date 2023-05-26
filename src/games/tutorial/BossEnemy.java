9a2b2647b7e67f003162e99463fd461d60dc982f
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-05-26 12:37:00 -0400

    changes indentation [Java]

--- src/games/tutorial/BossEnemy.java
+++ src/games/tutorial/BossEnemy.java
@@ -30,155 +30,155 @@ public class BossEnemy extends GameObject
 // defines Boss Enemy with attributes inherited from the Game Object Class
 {
 
-	/* Boss Enemy Attributes */
+  /* Boss Enemy Attributes */
 
-	private int ticks = 0;			// times boss fight
-	private ID spawns;			// enemy ID that it spawns
-	private Color color;			// enemy color
-	private boolean shape = false;		// enemy shape (rectangle)
-	private Handler handler;		// spawning handler
-
-
-	/* Constructors */
+  private int ticks = 0;			// times boss fight
+  private ID spawns;			// enemy ID that it spawns
+  private Color color;			// enemy color
+  private boolean shape = false;		// enemy shape (rectangle)
+  private Handler handler;		// spawning handler
 
 
-	public BossEnemy (int x, int y, ID id, Color color, int width,
-			  int height, ID spawns, Handler handler)
-	{
-		// creates boss enemy object
-		super (x, y, id, width, height);
-
-		// defines attributes
-		this.color = color;
-		this.spawns = spawns;
-		this.handler = handler;
-		v_x = 1;
-		v_y = 0;
-	}
+  /* Constructors */
 
 
-	/* Methods */
+  public BossEnemy (int x, int y, ID id, Color color, int width,
+      int height, ID spawns, Handler handler)
+  {
+    // creates boss enemy object
+    super (x, y, id, width, height);
+
+    // defines attributes
+    this.color = color;
+    this.spawns = spawns;
+    this.handler = handler;
+    v_x = 1;
+    v_y = 0;
+  }
 
 
-	public void shoot ()	/* unimplemented */
-	// we need to define this method because objects derived from the
-	// Game Object class must define the abstract methods of the class
-	{
-		return;
-	}
+  /* Methods */
 
 
-	public void tick ()
-	// initial tick method
-	{
-
-		// marks enemy as garbage if it has been destroyed,
-		// otherwise updates its position and velocity
-		if ( isDestroyed() )
-			setGarbage();	// sets garbage state to true
-		else
-		{
-			/* updates position */
-
-			x += v_x;
-			y += v_y;
-
-			/* simulates elastic collisions with boundaries */
-
-			if ( x <= 0 || x >= (Game.WIDTH - width) )
-				v_x *= -1;
-
-			if ( y <= 0 || y >= (Game.HEIGHT - 64) )
-				v_y *= -1;
-
-			/* fires random projectiles at player */
-			if (ticks != 0 && ticks % 50 == 0)
-				fire ();
+  public void shoot ()	/* unimplemented */
+    // we need to define this method because objects derived from the
+    // Game Object class must define the abstract methods of the class
+  {
+    return;
+  }
 
 
-			/* destroys boss to remove it from the game */
-			if (ticks != 0 && ticks % 1000 == 0)
-				setDestroyed();
+  public void tick ()
+    // initial tick method
+  {
 
-			++ticks;
-		}
+    // marks enemy as garbage if it has been destroyed,
+    // otherwise updates its position and velocity
+    if ( isDestroyed() )
+      setGarbage();	// sets garbage state to true
+    else
+    {
+      /* updates position */
 
-	}
+      x += v_x;
+      y += v_y;
 
+      /* simulates elastic collisions with boundaries */
 
-	public void render (Graphics g)
-	// initial render method
-	{
-		// sets enemy color
-		g.setColor (color);
+      if ( x <= 0 || x >= (Game.WIDTH - width) )
+	v_x *= -1;
 
-		// renders enemy according to its shape
-		if (shape)
-			g.fillOval (x, y, width, height);	// circle
-		else
-			g.fillRect (x, y, width, height);	// square
-	}
+      if ( y <= 0 || y >= (Game.HEIGHT - 64) )
+	v_y *= -1;
 
-
-	public Rectangle getBounds ()
-	// we need this for collision detection
-	{
-		return new Rectangle (x, y, width, height);
-	}
+      /* fires random projectiles at player */
+      if (ticks != 0 && ticks % 50 == 0)
+	fire ();
 
 
-	private void fire ()
-	// fires enemies at the player
-	{
+      /* destroys boss to remove it from the game */
+      if (ticks != 0 && ticks % 1000 == 0)
+	setDestroyed();
 
-		/* defines the attributes of the fired enemies */
+      ++ticks;
+    }
 
-		Random r = new Random ();	// PRNG for enemy speed
-		Color color = this.color;	// color
-		int posX = x + (width / 2);	// x-axis coordinate
-		int posY = y + (height / 2);	// y-axis coordinate
-		int minVel = 5, maxVel = 10;	// min and max velocity
-		int vel;			// velocity
-
-		switch (spawns)
-		// spawns enemies acording to the type that the boss spawns
-		{
-
-			case BasicEnemy:
-			// spawns basic enemy
-			BasicEnemy basic = new BasicEnemy (posX, posY,
-							   spawns, color,
-							   handler);
-
-			// sets basic enemy speed
-			vel = minVel + r.nextInt(maxVel - minVel);
-			basic.setVelX (vel);
-
-			vel = minVel + r.nextInt(maxVel - minVel);
-			basic.setVelY (vel);
-
-			handler.addObject (basic);
-			break;
+  }
 
 
-			case FastEnemy:
-			// spawns fast enemy
-			FastEnemy fast = new FastEnemy (posX, posY, spawns,
-							color, handler);
+  public void render (Graphics g)
+    // initial render method
+  {
+    // sets enemy color
+    g.setColor (color);
 
-			minVel = 10;
-			maxVel = 15;
-			// sets fast enemy speed
-			vel = minVel + r.nextInt(maxVel - minVel);
-			fast.setVelX (vel);
+    // renders enemy according to its shape
+    if (shape)
+      g.fillOval (x, y, width, height);	// circle
+    else
+      g.fillRect (x, y, width, height);	// square
+  }
 
-			vel = minVel + r.nextInt(maxVel - minVel);
-			fast.setVelY (vel);
 
-			handler.addObject (fast);
-			break;
-		}
-	}
+  public Rectangle getBounds ()
+    // we need this for collision detection
+  {
+    return new Rectangle (x, y, width, height);
+  }
+
+
+  private void fire ()
+    // fires enemies at the player
+  {
+
+    /* defines the attributes of the fired enemies */
+
+    Random r = new Random ();	// PRNG for enemy speed
+    Color color = this.color;	// color
+    int posX = x + (width / 2);	// x-axis coordinate
+    int posY = y + (height / 2);	// y-axis coordinate
+    int minVel = 5, maxVel = 10;	// min and max velocity
+    int vel;			// velocity
+
+    switch (spawns)
+      // spawns enemies acording to the type that the boss spawns
+    {
+
+      case BasicEnemy:
+	// spawns basic enemy
+	BasicEnemy basic = new BasicEnemy (posX, posY,
+	    spawns, color,
+	    handler);
+
+	// sets basic enemy speed
+	vel = minVel + r.nextInt(maxVel - minVel);
+	basic.setVelX (vel);
+
+	vel = minVel + r.nextInt(maxVel - minVel);
+	basic.setVelY (vel);
+
+	handler.addObject (basic);
+	break;
+
+
+      case FastEnemy:
+	// spawns fast enemy
+	FastEnemy fast = new FastEnemy (posX, posY, spawns,
+	    color, handler);
+
+	minVel = 10;
+	maxVel = 15;
+	// sets fast enemy speed
+	vel = minVel + r.nextInt(maxVel - minVel);
+	fast.setVelX (vel);
+
+	vel = minVel + r.nextInt(maxVel - minVel);
+	fast.setVelY (vel);
+
+	handler.addObject (fast);
+	break;
+    }
+  }
 }
 
 /*
