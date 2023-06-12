a4f87e62400d41e44924d0532e12319be51df4c0
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-06-12 12:55:31 -0400

    changes indentation [Java]

--- src/games/tutorial/KeyInput.java
+++ src/games/tutorial/KeyInput.java
@@ -29,121 +29,121 @@ public class KeyInput extends KeyAdapter
 // defines the key input class for processing input from the player
 {
 
-	// we use this boolean array to avert sticky player movements
-	private boolean [] keyDown = {false, false, false, false};
-	// as for other objects this class needs a handler
-	private Handler handler;
+  // we use this boolean array to avert sticky player movements
+  private boolean [] keyDown = {false, false, false, false};
+  // as for other objects this class needs a handler
+  private Handler handler;
 
 
-	/* Constructors */
+  /* Constructors */
 
 
-	public KeyInput (Handler handler)
-	{
-		this.handler = handler;
-	}
+  public KeyInput (Handler handler)
+  {
+    this.handler = handler;
+  }
 
 
-	/* Methods */
+  /* Methods */
 
 
-	@Override
-	public void keyPressed (KeyEvent e)
-	// moves player according to the pressed key
-	{
-		int key = e.getKeyCode();	// gets code of pressed key
+  @Override
+  public void keyPressed (KeyEvent e)
+  // moves player according to the pressed key
+  {
+    int key = e.getKeyCode();	// gets code of pressed key
 
-		// gets the player
-		GameObject object = handler.objects.get(0);
-
-
-		// complains if the player is not at the front of the
-		// linked-list
-		String errMSG = ("KeyInput(): expects the player at " +
-				 "the front of the linked-list");
-		if (object.getID() != ID.Player)
-			throw new RuntimeException(errMSG);
+    // gets the player
+    GameObject object = handler.objects.get(0);
 
 
-		// handles key events for player
-		switch (key)
-		// maps pressed keys to player movements
-		// sets velocity according to pressed key
-		{
-			case KeyEvent.VK_UP:
-				keyDown[0] = true;
-				object.setVelY (-5);
-				break;
-			case KeyEvent.VK_DOWN:
-				keyDown[1] = true;
-				object.setVelY (+5);
-				break;
-			case KeyEvent.VK_LEFT:
-				keyDown[2] = true;
-				object.setVelX (-5);
-				break;
-			case (KeyEvent.VK_RIGHT):
-				keyDown[3] = true;
-				object.setVelX (+5);
-				break;
-			case (KeyEvent.VK_SPACE):
-				object.shoot ();
-				break;
-			case (KeyEvent.VK_R):
-				HUD.HEALTH = 100;
-				break;
-		}
-	}
+    // complains if the player is not at the front of the
+    // linked-list
+    String errMSG = ("KeyInput(): expects the player at " +
+	"the front of the linked-list");
+    if (object.getID() != ID.Player)
+      throw new RuntimeException(errMSG);
 
 
-	@Override
-	public void keyReleased (KeyEvent e)
-	// restores player defaults according to the released key
-	{
-		int key = e.getKeyCode();	// gets released key code
+    // handles key events for player
+    switch (key)
+      // maps pressed keys to player movements
+      // sets velocity according to pressed key
+    {
+      case KeyEvent.VK_UP:
+	keyDown[0] = true;
+	object.setVelY (-5);
+	break;
+      case KeyEvent.VK_DOWN:
+	keyDown[1] = true;
+	object.setVelY (+5);
+	break;
+      case KeyEvent.VK_LEFT:
+	keyDown[2] = true;
+	object.setVelX (-5);
+	break;
+      case (KeyEvent.VK_RIGHT):
+	keyDown[3] = true;
+	object.setVelX (+5);
+	break;
+      case (KeyEvent.VK_SPACE):
+	object.shoot ();
+	break;
+      case (KeyEvent.VK_R):
+	HUD.HEALTH = 100;
+	break;
+    }
+  }
 
-		// gets the player
-		GameObject object = handler.objects.get(0);
+
+  @Override
+  public void keyReleased (KeyEvent e)
+  // restores player defaults according to the released key
+  {
+    int key = e.getKeyCode();	// gets released key code
+
+    // gets the player
+    GameObject object = handler.objects.get(0);
 
 
-		// complains if the player is not at the front of the
-		// linked-list
-		String errMSG = ("KeyInput(): expects the player at " +
-				 "the front of the linked-list");
-		if (object.getID() != ID.Player)
-			throw new RuntimeException(errMSG);
+    // complains if the player is not at the front of the
+    // linked-list
+    String errMSG = ("KeyInput(): expects the player at " +
+	"the front of the linked-list");
+    if (object.getID() != ID.Player)
+      throw new RuntimeException(errMSG);
 
 
-		// handles key events for player
-		switch (key)
-		// resets velocity to default (stationary)
-		{
-			case KeyEvent.VK_UP:
-				keyDown[0] = false;
-				break;
-			case KeyEvent.VK_DOWN:
-				keyDown[1] = false;
-				break;
-			case KeyEvent.VK_LEFT:
-				keyDown[2] = false;
-				break;
-			case (KeyEvent.VK_RIGHT):
-				keyDown[3] = false;
-				break;
-			case (KeyEvent.VK_SPACE):
-				break;
-			case (KeyEvent.VK_R):
-				break;
-		}
+    // handles key events for player
+    switch (key)
+      // resets velocity to default (stationary)
+    {
+      case KeyEvent.VK_UP:
+	keyDown[0] = false;
+	break;
+      case KeyEvent.VK_DOWN:
+	keyDown[1] = false;
+	break;
+      case KeyEvent.VK_LEFT:
+	keyDown[2] = false;
+	break;
+      case (KeyEvent.VK_RIGHT):
+	keyDown[3] = false;
+	break;
+      case (KeyEvent.VK_SPACE):
+	break;
+      case (KeyEvent.VK_R):
+	break;
+    }
 
-		// zeroes vertical movement
-		if (!keyDown[0] && !keyDown[1])
-			object.setVelY (0);
+    // zeroes vertical movement
+    if (!keyDown[0] && !keyDown[1])
+      object.setVelY (0);
 
-		// zeroes horizontal movement
-		if (!keyDown[2] && !keyDown[3])
-			object.setVelX (0);
-	}
+    // zeroes horizontal movement
+    if (!keyDown[2] && !keyDown[3])
+      object.setVelX (0);
+  }
 }
 
 
