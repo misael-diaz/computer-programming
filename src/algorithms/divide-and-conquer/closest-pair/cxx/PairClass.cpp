a06a4409a6f1ef26e7b596999466817b49c4e65d
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-07-01 22:58:34 -0400

    changes indentation [cxx]

--- src/algorithms/divide-and-conquer/closest-pair/cxx/PairClass.cpp
+++ src/algorithms/divide-and-conquer/closest-pair/cxx/PairClass.cpp
@@ -27,39 +27,39 @@ References:
 Pair::Pair ()
 // default constructor, constructs an infinitely separated pair of points
 {
-	// gets the binary floating-point representation of positive infinity
-	double inf = std::numeric_limits<double>::infinity();
-	this -> first = new Point();
-	this -> second = new Point(0, inf);
-	this -> distance = inf;
+  // gets the binary floating-point representation of positive infinity
+  double inf = std::numeric_limits<double>::infinity();
+  this -> first = new Point();
+  this -> second = new Point(0, inf);
+  this -> distance = inf;
 
-	// adds the dynamically allocated Point objects to `this' handler
-	this -> handler.add(this -> first);
-	this -> handler.add(this -> second);
+  // adds the dynamically allocated Point objects to `this' handler
+  this -> handler.add(this -> first);
+  this -> handler.add(this -> second);
 }
 
 
 Pair::Pair (Point* p, Point* q, double d) : distance(d)
 // constructs from a pair of (read-only) points and their distance
 {
-	if ( p -> compareTo(q) < 0 )
-	{
-		this -> first = p;
-		this -> second = q;
-	}
-	else
-	{
-		this -> first = q;
-		this -> second = p;
-	}
+  if ( p -> compareTo(q) < 0 )
+  {
+    this -> first = p;
+    this -> second = q;
+  }
+  else
+  {
+    this -> first = q;
+    this -> second = p;
+  }
 
-	/*
+  /*
 
-	NOTE:
-	We need not to add the Point objects P and Q to this handler, for these
-	objects must already be managed by a handler.
+  NOTE:
+  We need not to add the Point objects P and Q to this handler, for these
+  objects must already be managed by a handler.
 
-	*/
+  */
 }
 
 
@@ -73,7 +73,7 @@ Pair::~Pair () {}
 double Pair::getDistance () const
 // returns the separating distance of the points that comprise the pair
 {
-	return (this -> distance);
+  return (this -> distance);
 }
 
 
@@ -83,26 +83,26 @@ double Pair::getDistance () const
 void Pair::copy (const Pair *pair)
 // implements a copy method for copying the components of pair objects
 {
-	this -> first = pair -> first;
-	this -> second = pair -> second;
-	this -> distance = pair -> distance;
+  this -> first = pair -> first;
+  this -> second = pair -> second;
+  this -> distance = pair -> distance;
 }
 
 
 void Pair::print () const
 // prints info about the pair object
 {
-	// prints the coordinates of the first point on the console
-	std::cout << "first:" << std::endl;
-	this -> first -> print();
+  // prints the coordinates of the first point on the console
+  std::cout << "first:" << std::endl;
+  this -> first -> print();
 
-	// prints the coordinates of the second point on the console
-	std::cout << "second:" << std::endl;
-	this -> second -> print();
+  // prints the coordinates of the second point on the console
+  std::cout << "second:" << std::endl;
+  this -> second -> print();
 
-	// prints the separating distance of the points on the console
-	std::cout << std::scientific << std::setprecision(15)
-		<< "distance: " << (this -> distance) << std::endl;
+  // prints the separating distance of the points on the console
+  std::cout << std::scientific << std::setprecision(15)
+    << "distance: " << (this -> distance) << std::endl;
 }
 
 
@@ -122,10 +122,10 @@ boolean		true if the comprising points are equidistant, false otherwise
 
 */
 {
-	double d1 = this -> distance;
-	double d2 = pair -> distance;
+  double d1 = this -> distance;
+  double d2 = pair -> distance;
 
-	return (d1 == d2);
+  return (d1 == d2);
 }
 
 
@@ -147,9 +147,9 @@ compare		returns 0 if `this' pair is equal to the other `pair' (equidistant),
 
 */
 {
-	double d1 = this -> distance;
-	double d2 = pair -> distance;
-	return Comparator(d1, d2);
+  double d1 = this -> distance;
+  double d2 = pair -> distance;
+  return Comparator(d1, d2);
 }
 
 
@@ -173,15 +173,15 @@ constructing a Pair object we need not to consider other cases.
 
 */
 {
-	// checks if the first points are equal
-	bool first = (this -> first) -> equalTo(pair -> first);
-	// checks if the second points are equal
-	bool second = (this -> second) -> equalTo(pair -> second);
+  // checks if the first points are equal
+  bool first = (this -> first) -> equalTo(pair -> first);
+  // checks if the second points are equal
+  bool second = (this -> second) -> equalTo(pair -> second);
 
-	if (first && second)
-		return true;
-	else
-		return false;
+  if (first && second)
+    return true;
+  else
+    return false;
 }
 
 
@@ -191,12 +191,12 @@ constructing a Pair object we need not to consider other cases.
 Pair* pair::min(Pair* first, Pair* second)
 // returns (reference to) the smallest of the two Pair objects
 {
-	if ( first -> compareTo(second) )
-	{
-		return first;
-	}
-	else
-	{
-		return second;
-	}
+  if ( first -> compareTo(second) )
+  {
+    return first;
+  }
+  else
+  {
+    return second;
+  }
 }
