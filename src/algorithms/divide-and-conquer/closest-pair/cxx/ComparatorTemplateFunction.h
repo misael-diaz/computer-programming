701aec548ef35cce8939f6957690f3f56e7ab01d
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-07-03 16:36:25 -0400

    relocates synopsis [cxx]

--- src/algorithms/divide-and-conquer/closest-pair/cxx/ComparatorTemplateFunction.h
+++ src/algorithms/divide-and-conquer/closest-pair/cxx/ComparatorTemplateFunction.h
@@ -1,26 +1,6 @@
 #ifndef GUARD_AC_COMPARATOR_FUN
 #define GUARD_AC_COMPARATOR_FUN
 
-/*
-
-Algorithms and Complexity				   January 12, 2023
-IST 4310
-Prof M Diaz-Maldonado
-
-Synopsis:
-Defines the Generic Comparator Function.
-
-Copyright (c) 2023 Misael Diaz-Maldonado
-This file is released under the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-References:
-[0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
-
-*/
-
-
 template <typename T> int Comparator (T x1, T x2)
 // returns 0 if x1 == x2, returns 1 if x1 > x2, and returns -1 if x1 < x2
 {
@@ -39,3 +19,22 @@ template <typename T> int Comparator (T x1, T x2)
 }
 
 #endif
+
+/*
+
+Algorithms and Complexity				   January 12, 2023
+IST 4310
+Prof M Diaz-Maldonado
+
+Synopsis:
+Defines the Generic Comparator Function.
+
+Copyright (c) 2023 Misael Diaz-Maldonado
+This file is released under the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+References:
+[0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
+
+*/
