fd155052afb77fb071b7f7a09d8749533b5e4a0d
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-07-03 16:38:05 -0400

    changes indentation [cxx]

--- src/algorithms/divide-and-conquer/closest-pair/cxx/HandlerTemplateClass.h
+++ src/algorithms/divide-and-conquer/closest-pair/cxx/HandlerTemplateClass.h
@@ -8,99 +8,99 @@ template <typename T> class Handler
 // Generic Handler Class for handling dynamically allocated objects
 {
 
-	private:
+  private:
 
 
-	/* component(s) */
+    /* component(s) */
 
 
-	std::list<T> objects;		// objects placeholder
+    std::list<T> objects;		// objects placeholder
 
 
-	public:
+  public:
 
 
-	/* (de)constructors */
+    /* (de)constructors */
 
 
-	Handler () {}			// default constructor
+    Handler () {}			// default constructor
 
 
-	~Handler ()			// destructor
+    ~Handler ()			// destructor
+    {
+      destroy();
+    }
+
+
+    const std::list<T>& getObjects () const
+      // returns a reference to the vector of point objects
+    {
+      return (this -> objects);
+    }
+
+
+    size_t size () const
+      // returns the number of objects contained in the handler
+    {
+      return ( this -> objects.size() );
+    }
+
+
+    void add (T object)
+      // adds object to the handler
+    {
+      this -> objects.push_back(object);
+    }
+
+
+    void remove (T object)
+      // removes object from the handler
+    {
+      this -> objects.remove(object);
+    }
+
+
+    void erase ()
+      // erases all objects from the handler
+    {
+      this -> destroy();
+    }
+
+
+    bool contains (const T& target) const
+      // returns true if the target is present, returns false otherwise
+    {
+      const std::list<T>& objects = this -> objects;
+      for (T object : objects)
+      {
+	if ( object -> equalTo(target) )
 	{
-		destroy();
+	  return true;
 	}
+      }
+
+      return false;
+    }
 
 
-	const std::list<T>& getObjects () const
-	// returns a reference to the vector of point objects
-	{
-		return (this -> objects);
-	}
+  private:
 
 
-	size_t size () const
-	// returns the number of objects contained in the handler
-	{
-		return ( this -> objects.size() );
-	}
+    /* implementations */
 
 
-	void add (T object)
-	// adds object to the handler
-	{
-		this -> objects.push_back(object);
-	}
+    void destroy ()
+      // releases dynamically allocated objects from memory
+    {
+      for (T obj : objects)
+      {
+	delete obj;	// releases object from memory
+	obj = NULL;	// nullifies pointer to object
+      }
 
-
-	void remove (T object)
-	// removes object from the handler
-	{
-		this -> objects.remove(object);
-	}
-
-
-	void erase ()
-	// erases all objects from the handler
-	{
-		this -> destroy();
-	}
-
-
-	bool contains (const T& target) const
-	// returns true if the target is present, returns false otherwise
-	{
-		const std::list<T>& objects = this -> objects;
-		for (T object : objects)
-		{
-			if ( object -> equalTo(target) )
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-
-	private:
-
-
-	/* implementations */
-
-
-	void destroy ()
-	// releases dynamically allocated objects from memory
-	{
-		for (T obj : objects)
-		{
-			delete obj;	// releases object from memory
-			obj = NULL;	// nullifies pointer to object
-		}
-
-		// erases the nodes of the linked-list std::list
-		objects.erase( objects.begin(), objects.end() );
-	}
+      // erases the nodes of the linked-list std::list
+      objects.erase( objects.begin(), objects.end() );
+    }
 };
 
 #endif
