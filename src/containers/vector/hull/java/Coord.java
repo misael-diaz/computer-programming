36fef1e0eedbf3fd12febeacfbfc12b32b5bcd96
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-08-19 13:48:36 -0400

    moves synopsis to the end [Java]

--- src/containers/vector/hull/java/Coord.java
+++ src/containers/vector/hull/java/Coord.java
@@ -1,28 +1,3 @@
-/*
- * Algorithms and Complexity                            October 06, 2022
- * IST 4310
- * Prof. M. Diaz-Maldonado
- *
- *
- * Synopsis:
- * Possible implementation of a 2D Cartesian Coordinates Class.
- *
- *
- * Copyright (c) 2022 Misael Diaz-Maldonado
- * This file is released under the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- *
- * References:
- * [0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
- * [1] Sorting Custom Objects Tutorial: (
- *      www.codejava.net/java-core/collections/
- *      sorting-arrays-examples-with-comparable-and-comparator
- * )
- *
- */
-
 public class Coord implements Comparable<Coord>
 // 2D Cartesian Coordinates Class
 {
@@ -106,3 +81,29 @@ public class Coord implements Comparable<Coord>
 		return ( (xi - xj) * (xi - xj) + (yi - yj) * (yi - yj) );
 	}
 }
+
+
+/*
+ * Algorithms and Complexity                            October 06, 2022
+ * IST 4310
+ * Prof. M. Diaz-Maldonado
+ *
+ *
+ * Synopsis:
+ * Possible implementation of a 2D Cartesian Coordinates Class.
+ *
+ *
+ * Copyright (c) 2022 Misael Diaz-Maldonado
+ * This file is released under the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ * References:
+ * [0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example
+ * [1] Sorting Custom Objects Tutorial: (
+ *      www.codejava.net/java-core/collections/
+ *      sorting-arrays-examples-with-comparable-and-comparator
+ * )
+ *
+ */
