0938164a8a8c7cd870b65063e9659c1cb86fedc6
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-08-23 22:38:35 -0400

    finds the convex hull recursively [Java]
    COMMENTS:
    the convex hull found by the divide and conquer and brute force
    algorithms is the same for all the cases considered
    
    points are sampled from a pseudo-random number generator
    
    hopefully the code can be read by itself, comments were added in key
    codeblocks
    
    code passes new and existing test

--- src/containers/vector/hull/java/Vector.java
+++ src/containers/vector/hull/java/Vector.java
@@ -3,19 +3,111 @@ public final class Vector
   final double x;
   final double y;
 
-  Vector (Point P, Point Q)
+  Vector ()
+  {
+    this.x = 0.0;
+    this.y = 0.0;
+  }
+
+  Vector (final double x, final double y)
+  {
+    this.x = x;
+    this.y = y;
+  }
+
+  Vector (final Point P, final Point Q)
   {
     this.x = P.getX() - Q.getX();
     this.y = P.getY() - Q.getY();
   }
 
-  double dot (Vector vector)
+  public double dot (final Vector vector)
   {
     final double x1 = this.x;
     final double y1 = this.y;
     final double x2 = vector.x;
     final double y2 = vector.y;
     return ( x1 * x2 + y1 * y2 );
+  }
+
+  public static Vector prod (final Vector U, final double c)
+  {
+    final double x = c * U.x;
+    final double y = c * U.y;
+    return new Vector(x, y);
+  }
+
+  public static Vector sub (final Vector U, final Vector V)
+  {
+    final double x = U.x - V.x;
+    final double y = U.y - V.y;
+    return new Vector(x, y);
+  }
+
+  // yields ||U|^2 * Vt|^2, where Vt is the transversal component of V with respect to U
+  public static double trans (final Vector U, final Vector V)
+  {
+    Vector VI = prod(V, U.dot(U));	// V .dot. I, where I is the Identity tensor
+    Vector VUU = prod(U, V.dot(U));	// V .dot. UU dyad
+    Vector T = sub(VI, VUU);		// transversal component of V with respect to U
+    return T.dot(T);			// squared magnitude of the transversal vector
+  }
+
+
+  public static void main (String [] args)
+  {
+    test();
+    test1();
+    test2();
+  }
+
+
+  private static void test ()
+  {
+    Vector V = new Vector(1, 1);
+    System.out.print("test[0]: ");
+    if (trans(V, V) != 0)
+    {
+      System.out.println("FAIL");
+    }
+    else
+    {
+      System.out.println("PASS");
+    }
+  }
+
+
+  private static void test1 ()
+  {
+    Vector U = new Vector(0, 1);
+    Vector V = new Vector(1, 0);
+    System.out.print("test[1]: ");
+    if (trans(U, V) != 1)
+    {
+      System.out.println("FAIL");
+    }
+    else
+    {
+      System.out.println("PASS");
+    }
+  }
+
+
+  private static void test2 ()
+  {
+    Vector U = new Vector(+1, +1);
+    Vector V = new Vector(+1, -1);
+    // V and U are orthogonal so the expected result is the following
+    final double res = ( U.dot(U) * U.dot(U) ) * V.dot(V);
+    System.out.print("test[2]: ");
+    if (trans(U, V) != res)
+    {
+      System.out.println("FAIL");
+    }
+    else
+    {
+      System.out.println("PASS");
+    }
   }
 }
 
